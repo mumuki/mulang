6a1e311ced398a95bb3024d36319e056cb27e800
Federico Aloi <federico.aloi@gmail.com>
2016-02-24 01:26:31 -0300

    Need to set git credentials before commiting

--- escualo_before_deploy.sh
+++ escualo_before_deploy.sh
@@ -2,5 +2,7 @@
 
 cd dist/build/mulang
 git init
+git config user.email "travis@mumuki.org"
+git config user.name "Travis CI"
 git add mulang
 git commit -m "[Escualo::Travis] Deploy $TRAVIS_COMMIT" 
