93b05823af83fb90b09b226965f50c5593d32574
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-06-16 14:44:39 -0300

    Renaming declaresConditional to usesConditional

--- src/Language/Mulang/Inspector/Extras.hs
+++ src/Language/Mulang/Inspector/Extras.hs
@@ -1,13 +1,13 @@
 module Language.Mulang.Inspector.Extras (
-  declaresConditional,
+  usesConditional,
   declaresPredicate) where
 
 import Language.Mulang.Explorer
 import Language.Mulang.Inspector
 import Language.Mulang.Inspector.Combiner
 
-declaresConditional :: Inspection
-declaresConditional = alternative usesIf usesGuards
+usesConditional :: Inspection
+usesConditional = alternative usesIf usesGuards
 
 declaresPredicate :: BindingPredicate -> Inspection
 declaresPredicate pred = alternative (declaresFact pred) (declaresRule pred)
