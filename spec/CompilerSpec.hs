535acd53bac5557f678c71923b702fc31a0df8be
Federico Aloi <federico.aloi@gmail.com>
2016-06-30 21:31:47 -0300

    Added spec of JSON encoding

--- spec/CompilerSpec.hs
+++ spec/CompilerSpec.hs
@@ -5,8 +5,12 @@ import           Data.Aeson
 import           Language.Mulang.Cli.Compiler as C
 import           Test.Hspec
 
+expectationJson = LBS.pack "{\"subject\":[\"x\"],\"transitive\":false,\"negated\":true,\"object\":{\"tag\":\"Anyone\",\"contents\":[]},\"verb\":\"uses\"}"
+expectation = C.Expectation ["x"] "uses" Anyone True False
+
 spec = describe "Compiler" $ do
   it "can decode expectation JSON" $ do
-    let json = "{\"subject\":[\"x\"],\"transitive\":false,\"negated\":true,\"object\":{\"tag\":\"Anyone\",\"contents\":[]},\"verb\":\"uses\"}"
-    let decoded = (decode . LBS.pack) json :: Maybe C.Expectation
-    decoded `shouldBe` Just (C.Expectation ["x"] "uses" Anyone True False)
+    (decode expectationJson :: Maybe C.Expectation) `shouldBe` Just expectation
+
+  it "can encode expectation JSON" $ do
+    encode expectation `shouldBe` expectationJson
