56a773eed1066c4957890512cbb09ba97439e84a
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-07-07 18:50:22 -0300

    Improving error messages

--- src/Main.hs
+++ src/Main.hs
@@ -3,21 +3,22 @@
 module Main (main) where
 
 import Language.Mulang.Cli.Interpreter
-import Data.Aeson
-import System.Environment
-import Data.Maybe (fromJust)
+import Data.Aeson (eitherDecode, encode)
+import System.Environment (getArgs)
+import Control.Fallible (orFail)
 
 import qualified Data.ByteString.Lazy.Char8 as LBS (pack, putStrLn)
 
 main :: IO ()
 main = do
   body <- fmap parseArgs $ getArgs
-  LBS.putStrLn . encode . evaluate . fromJust . decode . LBS.pack $ body
+  LBS.putStrLn . encode . evaluate . decode . LBS.pack $ body
 
 parseArgs :: [String] -> String
 parseArgs [jsonBody] = jsonBody
 parseArgs _          = error usage
 
+decode = orFail . eitherDecode
 
 usage = "Wrong usage.                                 \n\
         \                                             \n\
