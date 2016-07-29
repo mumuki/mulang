97ce812dcaf22fc4a4ac0c01cd46af12dc921aa1
trimegisto <federico.lochbaum@gmail.com>
2016-07-29 10:54:56 -0300

    add path Gobstones.hs to mulang.cabal and add builder Program to Expression Mulang

--- spec/ConvertGobstonesAstToMulangAstSpec.hs
+++ spec/ConvertGobstonesAstToMulangAstSpec.hs
@@ -10,5 +10,5 @@ spec = do
 
 describe "translateProgramGobstonesToMulangExpression" $ do
     it "translate simple program Gobstones" $ do
-      let gobstonesAst =  json "{\"alias\": \"program\",\"body\": \"null\",\"from\": \"0\"}"
-      (translateGobstonesAst gobstonesAst :: GobstonesAst) `shouldBe` Program []
+      let gobstonesAst =  json "[{\"alias\": \"program\",\"body\": \"null\",\"from\": \"0\"}]"
+      (translateGobstonesAst gobstonesAst :: Expression) `shouldBe` Program []
