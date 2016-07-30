ede67688367d12edcaacc78e6c901611b0ba403a
trimegisto <federico.lochbaum@gmail.com>
2016-07-30 10:48:35 -0300

    Implementation FromJson of type GobstonesAst

--- spec/ConvertGobstonesAstToMulangAstSpec.hs
+++ spec/ConvertGobstonesAstToMulangAstSpec.hs
@@ -10,5 +10,6 @@ spec = do
 
 describe "translateProgramGobstonesToMulangExpression" $ do
     it "translate simple program Gobstones" $ do
-      let gobstonesAst =  json "[{\"alias\": \"program\",\"body\": \"null\",\"from\": \"0\"}]"
-      (translateGobstonesAst gobstonesAst :: Expression) `shouldBe` Program []
+      let gobstonesAst =  json  "[{\"alias\": \"program\",\"body\": \"null\",\"from\": \"0\"}]"
+
+      (translateGobstonesAst gobstonesAst) `shouldBe` Program []
