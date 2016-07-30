43d139be98fda3498b01d1de8b91788cc7db3e4f
trimegisto <federico.lochbaum@gmail.com>
2016-07-30 11:34:46 -0300

    function parseGobstones

--- spec/ConvertGobstonesAstToMulangAstSpec.hs
+++ spec/ConvertGobstonesAstToMulangAstSpec.hs
@@ -3,13 +3,12 @@ module ConvertGobstonesAstToMulangAstSpec (spec) where
 import           Test.Hspec
 import           Language.Mulang
 import			 Language.Mulang.Parsers.Gobstones
-import 			 Language.Mulang.Parsers.Json
 
 spec :: Spec
 spec = do
 
 describe "translateProgramGobstonesToMulangExpression" $ do
     it "translate simple program Gobstones" $ do
-      let gobstonesAst =  json  "[{\"alias\": \"program\",\"body\": \"null\",\"from\": \"0\"}]"
+      let gobstonesAst =  parseGobstones "[{\"alias\": \"program\",\"body\": \"null\",\"from\": \"0\"}]"
 
-      (translateGobstonesAst gobstonesAst) `shouldBe` Program []
+      gobstonesAst `shouldBe` Program []
