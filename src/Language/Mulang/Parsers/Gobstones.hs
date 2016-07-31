25214d8724ac7052f6c77773f1c22fa8cd991c7e
trimegisto <federico.lochbaum@gmail.com>
2016-07-31 00:23:32 -0300

    running simple program gobstones test and null program test

--- src/Language/Mulang/Parsers/Gobstones.hs
+++ src/Language/Mulang/Parsers/Gobstones.hs
@@ -10,10 +10,11 @@ import Control.Applicative
 import Data.Maybe (fromJust)
 import qualified Data.ByteString.Lazy.Char8 as LBS (pack)
 
+
 import GHC.Generics
 import Data.Text (Text)
 
-data GobstonesAst = AST [NodeAst] deriving (Show)
+data GobstonesAst = AST [NodeAst] | NullProgram deriving (Show)
 
 data NodeAst = Node { alias :: Alias   
                      , body :: Body 
@@ -28,17 +29,20 @@ type From = Int
 
 instance FromJSON GobstonesAst  where
 	parseJSON (Array list) =  (\a -> AST . toList <$> traverse parseJSON a) list
+	parseJSON Null = pure NullProgram
+	parseJSON _ = fail "Failed to parse GobstonesAst!"
 
 instance FromJSON NodeAst where
-	parseJSON (Object v) = Node <$> v .: "alias" <*> 
-                                    v .: "body" <*> 
-                                    v .: "from"
+	parseJSON (Object v) = Node <$> v .: "alias" <*> v .: "body" <*> v .: "from"
+	parseJSON _ = fail "Failed to parse NodeAst!"
 
 instance FromJSON Alias  where
 	parseJSON (String "program") = pure ProgramGobstones
+	parseJSON _ = fail "Failed to parse Alias!"
 
 instance FromJSON Body  where
 	parseJSON Null = pure NullP
+	parseJSON _ = fail "Failed to parse Body!"
 
 parseGobstones :: String -> Expression
 parseGobstones  = translateGobstonesAst . fromJust . parseGobstonesAst
@@ -47,7 +51,8 @@ parseGobstonesAst :: String -> Maybe GobstonesAst
 parseGobstonesAst = decode . LBS.pack
 
 translateGobstonesAst :: GobstonesAst -> Expression
-translateGobstonesAst  (AST ast) = Program []--(map translateNodeAst ast) --TODO : no estoy seguro si deberia ser asi o como abajo 
+translateGobstonesAst NullProgram = MuNull
+translateGobstonesAst  (AST ast) = Program (map translateNodeAst ast)
 
 translateNodeAst :: NodeAst -> Expression
-translateNodeAst (Node ProgramGobstones NullP _) = Program []
+translateNodeAst (Node ProgramGobstones NullP _) = Program []--ProgramDeclaration []
