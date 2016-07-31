dd5688d73c4086fd1b30dc227f9b60fcfbf5b2ff
trimegisto <federico.lochbaum@gmail.com>
2016-07-31 17:05:00 -0300

    Add ProgramDeclaration to GobstonesAst

--- src/Language/Mulang/Parsers/Gobstones.hs
+++ src/Language/Mulang/Parsers/Gobstones.hs
@@ -4,6 +4,7 @@ module Language.Mulang.Parsers.Gobstones (parseGobstones,GobstonesAst) where
 
 import Language.Mulang
 import Data.Aeson
+import Data.HashMap.Lazy (member)
 import Data.Traversable (traverse)
 import Data.Foldable (toList)
 import Control.Applicative
@@ -16,33 +17,88 @@ import Data.Text (Text)
 
 data GobstonesAst = AST [NodeAst] | NullProgram deriving (Show)
 
-data NodeAst = Node { alias :: Alias   
+data NodeAst = ProgramDeclaration  --TODO : falta refactor
+					{ alias :: Alias   
                      , body :: Body 
                      , from :: From 
+                     } 
+             | ProcedureDeclarationG 
+             		{ alias :: Alias   
+                     , body :: Body 
+                     , from :: From 
+                     , row :: Row
+					 , to :: To
+					 , value :: ValueP
+					 , arity :: ArityP 
+					 , reserved :: Reserved 
+					 , led :: Led 
+					 , lbp :: Lbp 
+					 , name :: Name 
+					 , parameters :: [Parameter]
                      } deriving (Show)
 
-data Alias = ProgramGobstones deriving (Show)
+data Alias = ProgramGobstones | ProcedureDeclarationA deriving (Show)
 
-data Body = NullP deriving (Show)
+data Body = NullP | Body { nodes :: [NodeAst]} deriving (Show)
+
+data Led = NullLed deriving (Show)
 
 type From = Int
 
-instance FromJSON GobstonesAst  where
+type Row = Int
+
+type To = Int
+
+type Lbp = Int
+
+type ValueP = String
+
+type ArityP = String
+
+type Reserved = Bool
+
+type Name = String
+
+data Parameter = P deriving (Show)
+
+------------------------------------------------------
+
+instance FromJSON GobstonesAst where
 	parseJSON (Array list) =  (\a -> AST . toList <$> traverse parseJSON a) list
 	parseJSON Null = pure NullProgram
 	parseJSON _ = fail "Failed to parse GobstonesAst!"
 
+parseNodeAst isProgramDeclaration isProcedureDeclaration value
+  | isProgramDeclaration = ProgramDeclaration <$> v .: "alias" <*> v .: "body" <*> v .: "from"
+  | isProcedureDeclaration = ProcedureDeclarationG <$> v .: "alias" <*> v .: "body" <*> v .: "from" <*> v .: "row" <*> v .: "to" <*> v .: "value" <*> v .: "arity" <*> v .: "reserved" <*> v .: "led" <*> v .: "lbp" <*> v .: "name" <*> v .: "parameters"
+  --| otherwise  = UmeUser     <$> parseUser value
+
 instance FromJSON NodeAst where
-	parseJSON (Object v) = Node <$> v .: "alias" <*> v .: "body" <*> v .: "from"
+	parseJSON (Object v) = nodeAst
+		where isProgramDeclaration = member "program" v
+			  isProcedureDeclaration = member "procedureDeclaration" v
+			  nodeAst = parseNodeAst isProgramDeclaration isProcedureDeclaration v
 	parseJSON _ = fail "Failed to parse NodeAst!"
 
 instance FromJSON Alias  where
 	parseJSON (String "program") = pure ProgramGobstones
+	parseJSON (String "procedureDeclaration") = pure ProcedureDeclarationA
 	parseJSON _ = fail "Failed to parse Alias!"
 
 instance FromJSON Body  where
+	parseJSON (Object v) = Body <$> v.: "nodes" 
 	parseJSON Null = pure NullP
 	parseJSON _ = fail "Failed to parse Body!"
+
+instance FromJSON Parameter where
+	parseJSON _ = pure P
+	parseJSON _ = fail "Failed to parse Parameter!"
+
+instance FromJSON Led where
+	parseJSON Null = pure NullLed
+	parseJSON _ = fail "Failed to parse Led!"
+
+------------------------------------------------------
 
 parseGobstones :: String -> Expression
 parseGobstones  = translateGobstonesAst . fromJust . parseGobstonesAst
@@ -52,7 +108,19 @@ parseGobstonesAst = decode . LBS.pack
 
 translateGobstonesAst :: GobstonesAst -> Expression
 translateGobstonesAst NullProgram = MuNull
-translateGobstonesAst  (AST ast) = Program (map translateNodeAst ast)
+translateGobstonesAst  (AST ast) = Sequence $ map convertToExpression ast 
 
-translateNodeAst :: NodeAst -> Expression
-translateNodeAst (Node ProgramGobstones NullP _) = Program []--ProgramDeclaration []
+convertToExpression :: NodeAst -> Expression
+convertToExpression n@(ProgramDeclaration _ _ _) = convertProgramToExpression n
+convertToExpression n@(ProcedureDeclarationG _ _ _ _ _ _ _ _ _ _ _ _) = convertProcedureToExpression n
+
+convertProgramToExpression :: NodeAst -> Expression
+convertProgramToExpression (ProgramDeclaration _ NullP _) = MuNull
+
+convertProcedureToExpression :: NodeAst -> Expression
+convertProcedureToExpression 
+			(ProcedureDeclarationG _ body _ _ _ _ arity _ _ _ name parameters) = ProcedureDeclaration name (convertParameters parameters)
+
+convertParameters :: [Parameter] -> [Expression]
+convertParameters [] = []
+
