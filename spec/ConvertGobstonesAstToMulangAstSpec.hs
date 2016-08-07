07d9fbc17877425f8a4be01c62af8091f5d2795e
trimegisto <federico.lochbaum@gmail.com>
2016-08-06 22:57:21 -0300

    Arguments types parsing, Colours, Directions, Booleans and Integers

--- spec/ConvertGobstonesAstToMulangAstSpec.hs
+++ spec/ConvertGobstonesAstToMulangAstSpec.hs
@@ -23,28 +23,39 @@ describe "translateProgramGobstonesToMulangExpression" $ do
       									
       gobstonesAst `shouldBe` Sequence [Sequence [Application (Variable "F") []]]
      
-     
     it "translate simple procedure declaration " $ do
       let gobstonesAst =  parseGobstones  "[{\"alias\": \"procedureDeclaration\",\"body\": null,\"from\": 1,\"row\": 1,\"to\": 12,\"value\": \"F\",\"arity\": \"routine\",\"reserved\": false,\"led\": null,\"lbp\": 0,\"name\": \"F\",\"parameters\": []}]"
 
       gobstonesAst `shouldBe` Sequence  [ProcedureDeclaration "F" [Equation [] (UnguardedBody MuNull)]]
 	
-
     it "translate simple procedure Application " $ do
       let gobstonesAst =  parseGobstones "[{\"alias\": \"program\",\"body\": [{\"alias\": \"ProcedureCall\",\"from\": 14,\"to\": 18,\"arity\": \"routine\",\"name\": \"F\",\"parameters\": []}],\"from\": 0},{\"alias\": \"procedureDeclaration\",\"body\": null,\"from\": 1,\"row\": 1,\"to\": 12,\"value\": \"F\",\"arity\": \"routine\",\"reserved\": false,\"led\": null,\"lbp\": 0,\"name\": \"F\",\"parameters\": []}]"
 
       gobstonesAst `shouldBe` Sequence [Sequence [Application (Variable "F") []],ProcedureDeclaration "F" [Equation [] (UnguardedBody MuNull)]]
-{-      
+      
     it "translate simple procedure declaration with a parameter" $ do
       let gobstonesAst =  parseGobstones "[{\"alias\": \"program\",\"body\": [{\"alias\": \"ProcedureCall\",\"from\": 14,\"to\": 18,\"arity\": \"routine\",\"name\": \"F\",\"parameters\": []}],\"from\": 0},{\"alias\": \"procedureDeclaration\",\"body\": null,\"from\": 1,\"row\": 1,\"to\": 12,\"value\": \"F\",\"arity\": \"routine\",\"reserved\": false,\"led\": null,\"lbp\": 0,\"name\": \"F\",\"parameters\":[{\"from\": 32,\"row\": 4,\"to\": 41,\"value\" : \"parameter\",\"arity\" : \"name\", \"reserved\": false,\"led\": null,\"std\": null,\"lbp\": 0}] }]"
 
       gobstonesAst `shouldBe` Sequence [Sequence [Application (Variable "F") []],ProcedureDeclaration "F" [Equation [VariablePattern "parameter"] (UnguardedBody MuNull)]]
      
     it "translate simple procedure declaration and application  with a parameter" $ do
-      let gobstonesAst =  parseGobstones "[{\"alias\": \"program\",\"body\": [{\"alias\": \"ProcedureCall\",\"from\": 14,\"to\": 18,\"arity\": \"routine\",\"name\": \"F\",\"parameters\": [{\"from\" : 15, \"row\" : 1, \"to\" : 16, \"value\" : 2, \"arity\" : \"literal\" }]}],\"from\": 0},{\"alias\": \"procedureDeclaration\",\"body\": null,\"from\": 1,\"row\": 1,\"to\": 12,\"value\": \"F\",\"arity\": \"routine\",\"reserved\": false,\"led\": null,\"lbp\": 0,\"name\": \"F\",\"parameters\":[{\"from\": 32,\"row\": 4,\"to\": 41,\"value\" : \"parameter\",\"arity\" : \"name\", \"reserved\": false,\"led\": null,\"std\": null,\"lbp\": 0}] }]"
+      let gobstonesAstNumber =  parseGobstones "[{\"alias\": \"program\",\"body\": [{\"alias\": \"ProcedureCall\",\"from\": 14,\"to\": 18,\"arity\": \"routine\",\"name\": \"F\",\"parameters\": [{\"from\" : 15, \"row\" : 1, \"to\" : 16, \"value\" : 2, \"arity\" : \"literal\" }]}],\"from\": 0},{\"alias\": \"procedureDeclaration\",\"body\": null,\"from\": 1,\"row\": 1,\"to\": 12,\"value\": \"F\",\"arity\": \"routine\",\"reserved\": false,\"led\": null,\"lbp\": 0,\"name\": \"F\",\"parameters\":[{\"from\": 32,\"row\": 4,\"to\": 41,\"value\" : \"parameter\",\"arity\" : \"name\", \"reserved\": false,\"led\": null,\"std\": null,\"lbp\": 0}] }]"
+      
+      let gobstonesAstColour =  parseGobstones "[{\"alias\": \"program\",\"body\": [{\"alias\": \"ProcedureCall\",\"from\": 14,\"to\": 18,\"arity\": \"routine\",\"name\": \"F\",\"parameters\": [{\"from\" : 15, \"row\" : 1, \"to\" : 16, \"value\" : 2,\"reserved\" : true , \"arity\" : \"literal\" }]}],\"from\": 0},{\"alias\": \"procedureDeclaration\",\"body\": null,\"from\": 1,\"row\": 1,\"to\": 12,\"value\": \"F\",\"arity\": \"routine\",\"reserved\": false,\"led\": null,\"lbp\": 0,\"name\": \"F\",\"parameters\":[{\"from\": 32,\"row\": 4,\"to\": 41,\"value\" : \"parameter\",\"arity\" : \"name\", \"reserved\": false,\"led\": null,\"std\": null,\"lbp\": 0}] }]"
 
-      gobstonesAst `shouldBe` Sequence [Sequence [Application (Variable "F") [MuNumber 2]],ProcedureDeclaration "F" [Equation [VariablePattern "parameter"] (UnguardedBody MuNull)]]
- 
+      let gobstonesAstBool =  parseGobstones "[{\"alias\": \"program\",\"body\": [{\"alias\": \"ProcedureCall\",\"from\": 14,\"to\": 18,\"arity\": \"routine\",\"name\": \"F\",\"parameters\": [{\"from\" : 15, \"row\" : 1, \"to\" : 16, \"value\" : true,\"reserved\" : true , \"arity\" : \"literal\" }]}],\"from\": 0},{\"alias\": \"procedureDeclaration\",\"body\": null,\"from\": 1,\"row\": 1,\"to\": 12,\"value\": \"F\",\"arity\": \"routine\",\"reserved\": false,\"led\": null,\"lbp\": 0,\"name\": \"F\",\"parameters\":[{\"from\": 32,\"row\": 4,\"to\": 41,\"value\" : \"parameter\",\"arity\" : \"name\", \"reserved\": false,\"led\": null,\"std\": null,\"lbp\": 0}] }]"
+      
+      let gobstonesAstDirection =  parseGobstones "[{\"alias\": \"program\",\"body\": [{\"alias\": \"ProcedureCall\",\"from\": 14,\"to\": 18,\"arity\": \"routine\",\"name\": \"F\",\"parameters\": [{\"from\" : 15, \"row\" : 1, \"to\" : 16, \"value\" : [1,0],\"reserved\" : true , \"arity\" : \"literal\" }]}],\"from\": 0},{\"alias\": \"procedureDeclaration\",\"body\": null,\"from\": 1,\"row\": 1,\"to\": 12,\"value\": \"F\",\"arity\": \"routine\",\"reserved\": false,\"led\": null,\"lbp\": 0,\"name\": \"F\",\"parameters\":[{\"from\": 32,\"row\": 4,\"to\": 41,\"value\" : \"parameter\",\"arity\" : \"name\", \"reserved\": false,\"led\": null,\"std\": null,\"lbp\": 0}] }]"
+
+      gobstonesAstNumber `shouldBe` Sequence [Sequence [Application (Variable "F") [MuNumber 2]],ProcedureDeclaration "F" [Equation [VariablePattern "parameter"] (UnguardedBody MuNull)]]
+
+      gobstonesAstColour `shouldBe` Sequence [Sequence [Application (Variable "F") [MuSymbol "Negro"]],ProcedureDeclaration "F" [Equation [VariablePattern "parameter"] (UnguardedBody MuNull)]]
+
+      gobstonesAstBool `shouldBe` Sequence [Sequence [Application (Variable "F") [MuBool True]],ProcedureDeclaration "F" [Equation [VariablePattern "parameter"] (UnguardedBody MuNull)]]
+
+      gobstonesAstDirection `shouldBe` Sequence [Sequence [Application (Variable "F") [MuSymbol "Este"]],ProcedureDeclaration "F" [Equation [VariablePattern "parameter"] (UnguardedBody MuNull)]]
+ 	
+ 	{-
 	it "translate simple variable assignment" $ do
       let gobstonesAst =  parseGobstones "[{\"alias\": \"program\",\"body\": [{\"alias\" : \":=\",\"arity\" : \"binary\", \"variable\" : {\"from\" : 13, \"row\" : 1,\"to\" : 14, \"value\" : \"x\", \"arity\" : \"name\"}, \"expression\" : {\"from\" : 18, \"row\" : 1,\"to\" : 19, \"value\" : 1, \"arity\" : \"literal\"}, \"assignment\" : true, \"from\" : 13,\"to\" : 20 }],\"from\": 0}]"
 
--- src/Language/Mulang/Parsers/Gobstones.hs
+++ src/Language/Mulang/Parsers/Gobstones.hs
@@ -13,14 +13,16 @@ import	qualified Data.ByteString.Lazy.Char8 as LBS (pack)
 import	qualified Data.Text as T
 import	GHC.Generics
 import	Data.Text (Text)
-import	Language.Mulang.Builder
+import	Language.Mulang.Builder as Builder
 import	qualified Data.Vector as V
+import	Data.Scientific as Scientific
+
 
 
 instance FromJSON Expression where
 	  parseJSON  =  parseBodyExpression 
 
-parseBodyExpression (Array list) = (\a -> Sequence . toList <$> traverse parseNodes a) list
+parseBodyExpression (Array list) = Builder.normalize <$> (\a -> Sequence . toList <$> traverse parseNodes a) list
 parseBodyExpression Null = pure MuNull
 parseBodyExpression _ = fail "Failed to parse Expression!"
 
@@ -31,20 +33,44 @@ parseNodes (Object v) = nodeAst
 
 parseParametersPatterns (Array list) 
 									| (V.null list) = pure []
-									| otherwise = pure []--(\a -> toList <$> parseParameterPatterns a) list
+									| otherwise = (\a -> toList <$> traverse parseParameterPatterns a) list
 
-
-parseParameterPatterns (Object value) = VariablePattern <$> (lookupAndParseExpression parseNameExpression "name" value)
+parseParameterPatterns (Object value) = VariablePattern <$> (lookupAndParseExpression parseNameExpression "value" value)
 
 
 parseParametersExpression (Array list) 
 									| (V.null list) = pure []
-									| otherwise = pure []--(\a ->  toList <$> parseParameterExpression a) list
+									| otherwise = (\a -> toList <$> traverse parseParameterExpression a) list
+
+parseParameterExpression (Object value) = parseParameterValue (lookUpValue "value" value) (HashMap.lookup "reserved" value)
+
+parseParameterValue n@(Number number) Nothing = MuNumber <$> (parseJSON n)
+parseParameterValue b@(Bool bool) _ = MuBool <$> (parseJSON b)
+parseParameterValue (Array direction) _ = MuSymbol <$> (parseListToDirection direction)--(\a -> MuSymbol . toList <$> traverse parseJSON a) direction--
+parseParameterValue (Number number) _ = MuSymbol <$> (parseToColour number)
+
+parseToColour number = case (Scientific.floatingOrInteger number) 
+						of 	(Right 1) -> parseJSON "Rojo"
+							(Right 2) -> parseJSON "Negro"
+							(Right 3) -> parseJSON "Azul"
+							(Right 4) -> parseJSON "Verde"
+
+parseListToDirection direction = let (Number n1,Number n2) = (V.head direction,V.last direction) 
+								 in parseToDirection n1 n2
+	where 
+		parseToDirection number1 number2 = case ((Scientific.floatingOrInteger number1),(Scientific.floatingOrInteger number2))
+											of	((Right 1),(Right 0)) -> parseJSON "Este"
+												((Right 0),(Right 1)) -> parseJSON "Norte"
+												((Right (-1)),(Right 0)) -> parseJSON "Oeste"
+												((Right 0),(Right (-1))) -> parseJSON "Sur"
 
 parseNameExpression (String n) = pure (T.unpack n) 
 
+lookUpValue :: Text -> Object -> Value
+lookUpValue string value = fromJust (HashMap.lookup string value)
+
 lookupAndParseExpression :: (Value -> b) -> Text -> Object -> b
-lookupAndParseExpression parseFunction string value= parseFunction ( fromJust (HashMap.lookup string value))
+lookupAndParseExpression parseFunction string value= parseFunction (lookUpValue string value)
  
 parseNodeAst (Just "program") value = lookupAndParseExpression parseBodyExpression "body" value
 parseNodeAst (Just "procedureDeclaration") value = ProcedureDeclaration <$> (lookupAndParseExpression parseNameExpression "name" value) <*> ((\x -> [x]) <$> (Equation <$> (lookupAndParseExpression parseParametersPatterns "parameters" value) <*> (UnguardedBody <$> (lookupAndParseExpression  parseBodyExpression "body" value))))
