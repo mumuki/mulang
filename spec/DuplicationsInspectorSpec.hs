545ee73aa4a9482c22d74a127a99b9909137093f
Fede <federico.lochbaum@gmail.com>
2016-09-29 22:20:45 -0300

    take literal expressions in DuplicateCode

--- spec/DuplicationsInspectorSpec.hs
+++ spec/DuplicationsInspectorSpec.hs
@@ -17,6 +17,29 @@ spec = do
         let code = "function F(){} function G(){Sacar(Verde)}"
         hasDuplicateCode (js code) `shouldBe` False
 
+      it "is False when two functions have different return" $ do
+        let code = "function F(){ return 4 } function G(){ return 45 }"
+        hasDuplicateCode (js code) `shouldBe` False
+      
+      it "is False when two functions have same literal return " $ do
+        let code = "function f(){ Sacar(Verde) return 2 } function g(){return 2 }"
+        hasDuplicateCode (js code) `shouldBe` False
+
+      describe "is False when two computations have marginal similities" $ do
+        it "different operations on same variable in return" $ do 
+          hasDuplicateCode (js "function F(){return x + 1 } function G(){ return x - 1  }") `shouldBe` False
+
+        it "only similar increments in body" $ do 
+          hasDuplicateCode (js "function F(){ x = x  + 1 ;  return x + 1 } function G(){ x = x  + 1 ; return x - 1  }") `shouldBe` False
+
+        it "only similar procedure calls in body" $ do 
+          hasDuplicateCode (js "function F(){ Mover(Sur) ; Poner(Rojo) ; Mover(Norte) }\
+                                \function G(){ Mover(Sur) ; Poner(Verde) ; Mover(Este) }") `shouldBe` False
+
+      it "is False when two functions have different body" $ do
+        let code = "function f(){ Sacar(Verde) return 2 } function g(){return 3 }"
+        hasDuplicateCode (js code) `shouldBe` False
+
       it "is True when two functions are equals" $ do
         let code = "function f(){if(true){}else{} return x} function g(){if(true){}else{} return x}"
         hasDuplicateCode (js code) `shouldBe` True
@@ -29,17 +52,12 @@ spec = do
         let code = "function f(){ Sacar(Verde) return 2 } function g(){ Sacar(Verde) return 2 }"
         hasDuplicateCode (js code) `shouldBe` True
 
-      it "is True when two functions have same return" $ do
-        let code = "function f(){ Sacar(Verde) return 2 } function g(){return 2 }"
-        hasDuplicateCode (js code) `shouldBe` True
-
-      it "is False when two functions have not same return" $ do
-        let code = "function f(){ Sacar(Verde) return 2 } function g(){return 3 }"
-        hasDuplicateCode (js code) `shouldBe` False
 
       it "is True when two functions have same expressions" $ do
         let code = "function f(){ Sacar(Verde) return x == y } function g(){ while(x==y){} return 2 }"
         hasDuplicateCode (js code) `shouldBe` True
+
+
 
 
 
--- src/Language/Mulang/DuplicateCode.hs
+++ src/Language/Mulang/DuplicateCode.hs
@@ -15,14 +15,20 @@ hasDuplicateCode x             = hasDuplicateInList [x] [] []
 --Falta generalizar los nombres de los parametros y las varaibles usadas en la busqueda de codigo repetido
 hasDuplicateInList :: [Expression] -> [[Equation]] -> [Expression] -> Bool
 hasDuplicateInList [] bodyL expL                                             = False
-hasDuplicateInList ((ProcedureDeclaration _ eq):xs) bodyL expL               = let exps = (getExpressionsInEquation eq) in (elem eq bodyL) || (not (null (intersect exps expL))) || hasDuplicateInList xs (eq : bodyL) (exps ++ expL)
-hasDuplicateInList ((FunctionDeclaration _ eq):xs) bodyL expL                = let exps = (getExpressionsInEquation eq) in (elem (filterReturn eq) bodyL) || (not (null (intersect exps expL))) || hasDuplicateInList xs (filterReturn eq : bodyL) (exps ++ expL)
+hasDuplicateInList ((ProcedureDeclaration _ eq):xs) bodyL expL               = let exps = (getExpressionsInEquation eq) in 
+                                                                                          (elem eq bodyL) || (not (null (intersect exps expL))) || hasDuplicateInList xs (eq : bodyL) (exps ++ expL)
+hasDuplicateInList ((FunctionDeclaration _ eq):xs) bodyL expL                = let exps = (getExpressionsInEquation eq) in
+                                                                                          (elem (filterReturn eq) bodyL) || (not (null (intersect exps expL))) || hasDuplicateInList xs (addEqsInBodyL (filterReturn eq) bodyL) (exps ++ expL)
 hasDuplicateInList (x:xs) bodyL expL                                         = hasDuplicateInList xs bodyL expL
 
+addEqsInBodyL :: [Equation] -> [[Equation]] -> [[Equation]]
+addEqsInBodyL [] ys = ys
+addEqsInBodyL l@(x:xs) ys = l : ys
 
 filterReturn :: [Equation] -> [Equation]
 filterReturn []                           = []
-filterReturn ((Equation patterns eqB):xs) = Equation patterns (filterReturnInEqBody eqB) : filterReturn xs 
+filterReturn ((Equation patterns eqB):xs) | UnguardedBody MuNull <- (filterReturnInEqBody eqB) = filterReturn xs
+                                          | otherwise = (Equation patterns (filterReturnInEqBody eqB)) : filterReturn xs
  
 filterReturnInEqBody :: EquationBody -> EquationBody
 filterReturnInEqBody (UnguardedBody (Sequence xs)) = UnguardedBody $ simplify (Sequence $ init xs) -- el return es el ultimo elemento en Gobstones
@@ -34,17 +40,25 @@ getExpressionsInEquation []                           = []
 getExpressionsInEquation ((Equation patterns eqB):xs) = getExpressionsEqBody eqB ++ getExpressionsInEquation xs
 
 getExpressionsEqBody :: EquationBody -> [Expression]
-getExpressionsEqBody (UnguardedBody (Sequence xs)) = getExpressions xs  
-getExpressionsEqBody (UnguardedBody e)             = getExpressions [e]
+getExpressionsEqBody (UnguardedBody (Sequence xs)) = takeLiterals $ getExpressions xs  
+getExpressionsEqBody (UnguardedBody e)             = takeLiterals $ getExpressions [e]
 
 getExpressions :: [Expression] -> [Expression]  
-getExpressions []                             = []
-getExpressions ((Return  e):xs)               = e : getExpressions xs
-getExpressions ((If  e b1 b2):xs)             = e : getExpressions xs ++ getExpressions [b1] ++ getExpressions [b2]
-getExpressions ((While  e b):xs)              = e : getExpressions xs ++ getExpressions [b]
-getExpressions ((Repeat  e b):xs)             = e : getExpressions xs ++ getExpressions [b]
-getExpressions ((VariableDeclaration i e):xs) = e : getExpressions xs
-getExpressions ((VariableAssignment i e):xs)  = e : getExpressions xs
-getExpressions ((Switch e cases):xs)          = e : getExpressions xs
-getExpressions (x:xs)                         = getExpressions xs
+getExpressions []                                 = []
+getExpressions ((Return  e):xs)                   = e : getExpressions xs
+getExpressions ((If  e b1 b2):xs)                 = e : getExpressions xs ++ getExpressions [b1] ++ getExpressions [b2]
+getExpressions ((While  e b):xs)                  = e : getExpressions xs ++ getExpressions [b]
+getExpressions ((Repeat  e b):xs)                 = e : getExpressions xs ++ getExpressions [b]
+getExpressions ((VariableDeclaration i e):xs)     = e : getExpressions xs
+getExpressions ((VariableAssignment i e):xs)      = e : getExpressions xs
+getExpressions ((Switch e cases):xs)              = e : getExpressions xs
+getExpressions ((Application e exps):xs)          = exps ++ getExpressions xs
+getExpressions (x:xs)                             = getExpressions xs
 
+takeLiterals :: [Expression] -> [Expression]
+takeLiterals []                  = []
+takeLiterals ((MuSymbol _):xs)   = takeLiterals xs
+takeLiterals ((MuNumber _):xs)   = takeLiterals xs
+takeLiterals ((MuBool _):xs)     = takeLiterals xs
+takeLiterals ((Variable _):xs)   = takeLiterals xs
+takeLiterals (x:xs)              = x : takeLiterals xs
