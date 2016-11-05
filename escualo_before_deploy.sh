095761115b34a960bd83addfebed0d8aea5c28e1
Franco Leonardo Bulgarelli <flbulgarelli@users.noreply.github.com>
2016-11-05 20:25:46 -0300

    Removing odd escualo file

--- escualo_before_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd dist/build/mulang
-git init
-git config user.email "travis@mumuki.org"
-git config user.name "Travis CI"
-git add mulang
-git commit -m "[Escualo::Travis] Deploy $TRAVIS_COMMIT" 
