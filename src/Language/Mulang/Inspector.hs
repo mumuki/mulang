19f301e873d884f7bfeb32ead9df696324185273
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2017-05-26 09:45:40 -0300

    Extracting Procedural module
    Fixes https://github.com/mumuki/mulang/issues/60

--- src/Language/Mulang/Inspector.hs
+++ src/Language/Mulang/Inspector.hs
@@ -4,48 +4,12 @@ module Language.Mulang.Inspector (
   module Language.Mulang.Inspector.ObjectOriented,
   module Language.Mulang.Inspector.Functional,
   module Language.Mulang.Inspector.Logic,
-
--- imperative
-  usesRepeat,
-  usesWhile,
-  usesSwitch,
-
-  declaresProcedure) where
+  module Language.Mulang.Inspector.Procedural) where
 
 
-import Language.Mulang.Ast
-import Language.Mulang.Binding
 import Language.Mulang.Inspector.Generic
 import Language.Mulang.Inspector.Combiner
 import Language.Mulang.Inspector.ObjectOriented
 import Language.Mulang.Inspector.Functional
 import Language.Mulang.Inspector.Logic
-
-
-declaresProcedure :: BindingPredicate -> Inspection
-declaresProcedure = containsDeclaration f
-  where f (ProcedureDeclaration _ _) = True
-        f _                          = False
-
-
--- | Inspection that tells whether an expression uses while
--- in its definition
-usesWhile :: Inspection
-usesWhile = containsExpression f
-  where f (While _ _) = True
-        f _ = False
--- | Inspection that tells whether an expression uses Switch
--- in its definition
-usesSwitch :: Inspection
-usesSwitch = containsExpression f
-  where f (Switch _ _) = True
-        f _ = False
-
--- | Inspection that tells whether an expression uses reoeat
--- in its definition
-usesRepeat :: Inspection
-usesRepeat = containsExpression f
-  where f (Repeat _ _) = True
-        f _ = False
-
-
+import Language.Mulang.Inspector.Procedural
---
+++ src/Language/Mulang/Inspector/Procedural.hs
@@ -0,0 +1,36 @@
+module Language.Mulang.Inspector.Procedural (
+  usesRepeat,
+  usesWhile,
+  usesSwitch,
+  declaresProcedure) where
+
+import Language.Mulang.Ast
+import Language.Mulang.Binding
+import Language.Mulang.Inspector.Generic
+
+declaresProcedure :: BindingPredicate -> Inspection
+declaresProcedure = containsDeclaration f
+  where f (ProcedureDeclaration _ _) = True
+        f _                          = False
+
+
+-- | Inspection that tells whether an expression uses while
+-- in its definition
+usesWhile :: Inspection
+usesWhile = containsExpression f
+  where f (While _ _) = True
+        f _ = False
+-- | Inspection that tells whether an expression uses Switch
+-- in its definition
+usesSwitch :: Inspection
+usesSwitch = containsExpression f
+  where f (Switch _ _) = True
+        f _ = False
+
+-- | Inspection that tells whether an expression uses reoeat
+-- in its definition
+usesRepeat :: Inspection
+usesRepeat = containsExpression f
+  where f (Repeat _ _) = True
+        f _ = False
+
