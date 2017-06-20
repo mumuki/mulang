a135c514efca1ed32a700948b479158217663ca8
Franco Bulgarelli <franco@mumuki.org>
2017-06-20 01:15:30 -0300

    Adding TokenizerSpec

---
+++ spec/TokenizerSpec.hs
@@ -0,0 +1,13 @@
+module TokenizerSpec (spec) where
+
+import           Test.Hspec
+import           Text.Inflections.Tokenizer (snakeCase, camelCase, tokenize)
+
+spec :: Spec
+spec = do
+  describe "tokenize" $ do
+    it "can tokenize camelCase words" $ do
+      tokenize camelCase "todayIsAGreatDay" `shouldBe` ["today", "is", "a", "great", "day"]
+
+    it "can tokenize snakeCase words" $ do
+      tokenize snakeCase "the_first_and_the_last" `shouldBe` ["the", "first", "and", "the", "last"]
