51ec808b0fb8f20c0b4bf6c785b5620df034f17c
Franco Bulgarelli <franco@mumuki.org>
2017-08-01 01:41:59 -0300

    Adding some comments

--- spec/InspectorSpec.hs
+++ spec/InspectorSpec.hs
@@ -295,6 +295,13 @@ spec = do
     it "is False when typed with another type, in java" $ do
       typesReturnAs (named "double") (java "class Foo { int f() { return 4; } }")  `shouldBe` False
 
+  -- typesAs => (TypeSignature _ [] n) | predicate n
+  -- typesReturnAs => (TypeSignature _ _ n) | predicate n
+  -- typesParametersAs => (TypeSignature _ pts _ | predicate . mkSignature $ n
+  -- typesAttributeAs => (Sequence [s, a]) = typesAs p s && declaresAttribute anyone a
+  -- typesVariableAs
+  --
+
   describe "declaresAttribute" $ do
     it "is True when present" $ do
       declaresAttribute (named "x") (js "var f = {x: 6}")  `shouldBe` True
