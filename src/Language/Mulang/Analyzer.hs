3badd2010953f21e751969d3add57d54b851dfd5
Franco Bulgarelli <franco@mumuki.org>
2017-11-08 12:30:53 -0300

    Analyzing includeIR flag

--- src/Language/Mulang/Analyzer.hs
+++ src/Language/Mulang/Analyzer.hs
@@ -22,7 +22,7 @@ import Language.Mulang.Analyzer.SignaturesAnalyzer  (analyseSignatures)
 import Language.Mulang.Analyzer.ExpectationsAnalyzer (analyseExpectations)
 import Language.Mulang.Analyzer.SmellsAnalyzer (analyseSmells)
 import Language.Mulang.Analyzer.DomainLanguageCompiler (emptyDomainLanguage, compileDomainLanguage)
-
+import Data.Maybe (fromMaybe)
 --
 -- Builder functions
 --
@@ -64,5 +64,9 @@ analyseAst ast spec = do
   return $ AnalysisCompleted (analyseExpectations ast (expectations spec))
                              (analyseSmells ast language (smellsSet spec))
                              (analyseSignatures ast (signatureAnalysisType spec))
-                             (Nothing)
+                             (analyzeIntermediateLanguage ast spec)
 
+analyzeIntermediateLanguage :: Expression -> AnalysisSpec -> Maybe Expression
+analyzeIntermediateLanguage ast spec
+  | fromMaybe False (includeIntermediateLanguage spec) = Just ast
+  | otherwise = Nothing
