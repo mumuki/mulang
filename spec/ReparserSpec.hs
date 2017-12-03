4761f62162d9993d38a64dfbf006694481d7ad24
Franco Bulgarelli <franco@mumuki.org>
2017-12-03 19:40:06 -0300

    Supporting braces and parens

--- spec/ReparserSpec.hs
+++ spec/ReparserSpec.hs
@@ -16,10 +16,10 @@ data SyntaxError
 
 data SyntaxErrorKind
   = UnclosedBracket
-  | UnclosedParenthesis
+  | UnclosedParen
   | UnclosedBrace
   | UnopenBracket
-  | UnopenParenthesis
+  | UnopenParen
   | UnopenBrace
   | BrokenQuote
   | BrokenDoubleQuote
@@ -44,26 +44,34 @@ program = strip False
 
 strip :: Bool -> ParsecParser [SyntaxError]
 strip nested = fmap concat $ many (choice choices)
-  where choices | nested = [stream, openBracket]
-                | otherwise = [stream, openBracket, unopenBracket]
+  where choices | nested = commonParsers
+                | otherwise = commonParsers ++ [unopen bracketMode, unopen parenMode, unopen braceMode]
 
-openBracket = do
+        commonParsers = [stream, open bracketMode, open parenMode, open braceMode]
+
+data Mode = Mode { l :: ParsecParser Char, r :: ParsecParser Char, uo :: SyntaxErrorKind, uc :: SyntaxErrorKind }
+
+parenMode = Mode lparen rparen UnopenParen UnclosedParen
+bracketMode = Mode lbracket rbracket UnopenBracket UnclosedBracket
+braceMode = Mode lbrace rbrace UnopenBrace UnclosedBrace
+
+open mode = do
   pos <- getPosition
-  lbracket
+  l mode
   optionMaybe (strip True)
-  choice [endBracked, unclosedBracket pos]
+  choice [end mode, unclosed mode pos]
 
-endBracked = do
-  rbracket
+end mode = do
+  r mode
   return []
 
-unopenBracket = do
+unopen mode = do
   pos <- getPosition
-  rbracket
-  return [SyntaxError (sourceLine pos) (sourceColumn pos) UnopenBracket]
+  r mode
+  return [SyntaxError (sourceLine pos) (sourceColumn pos) (uo mode)]
 
-unclosedBracket pos = do
-  return [SyntaxError (sourceLine pos) (sourceColumn pos) UnclosedBracket]
+unclosed mode pos = do
+  return [SyntaxError (sourceLine pos) (sourceColumn pos) (uc mode)]
 
 stream = do
   many1 letter
@@ -113,3 +121,51 @@ spec = do
 
     it "`hello[world]hello[world]`" $ do
       reparse "hello[world]hello[world]" `shouldBe` []
+
+    it "`return(bar)`" $ do
+      reparse "return(bar)" `shouldBe` []
+
+    it "`()`" $ do
+      reparse "()" `shouldBe` []
+
+    it "`()()`" $ do
+      reparse "()()" `shouldBe` []
+
+    it "works with letters between brackets" $ do
+      reparse "(hello)" `shouldBe` []
+
+    it "(" $ do
+      reparse "(" `shouldBe` [SyntaxError 1 1 UnclosedParen]
+
+    it "works with unclosed bracket and streams" $ do
+      reparse "(hello" `shouldBe` [SyntaxError 1 1 UnclosedParen]
+
+    it "`return(bar`" $ do
+      reparse "return(bar" `shouldBe` [SyntaxError 1 7 UnclosedParen]
+
+    it "`)`" $ do
+      reparse ")" `shouldBe` [SyntaxError 1 1 UnopenParen]
+
+    it "works with many trivial unbalanced brackets, on first pair" $ do
+      reparse "(()" `shouldBe` [SyntaxError 1 1 UnclosedParen]
+
+    it "works with mixed streams and brackets" $ do
+      reparse "hello(world(hello)world" `shouldBe` [SyntaxError 1 6 UnclosedParen]
+
+    it "works with nested streams and brackets" $ do
+      reparse "hello(world(hello)world)" `shouldBe` []
+
+    it "`hello(world(hello(world)`" $ do
+      reparse "hello(world(hello(world)" `shouldBe` [SyntaxError 1 6 UnclosedParen]
+
+    it "`hello(world)hello(world)`" $ do
+      reparse "hello(world)hello(world)" `shouldBe` []
+
+    it "`([])`" $ do
+      reparse "([])" `shouldBe` []
+
+    it "`{[(hello)]}`" $ do
+      reparse "{[(hello)]}" `shouldBe` []
+
+    it "`(]`" $ do
+      reparse "(]" `shouldBe` [SyntaxError 1 1 UnclosedParen, SyntaxError 1 2 UnopenBracket]
