9f966a816898b5b89a76a8b2d905efe13822028a
Franco Bulgarelli <franco@mumuki.org>
2017-12-03 23:00:08 -0300

    Properly supporting nesting

--- spec/ReparserSpec.hs
+++ spec/ReparserSpec.hs
@@ -5,9 +5,9 @@ module ReparserSpec (spec) where
 import           Test.Hspec
 
 import Text.Parsec
-import Text.Parsec.Expr
-import Text.Parsec.Numbers
 import Control.Fallible (orFail)
+import Control.Monad (msum, join)
+import Data.Maybe (isJust)
 
 type ParsecParser a = forall x . Parsec String x a
 
@@ -36,136 +36,188 @@ rbrace = char '}'
 lbracket = char '['
 rbracket = char ']'
 
-reparse :: String -> [SyntaxError]
-reparse = orFail . parse (program <* eof) ""
+type Reparser = ParsecParser (Maybe SyntaxError)
 
-program :: ParsecParser [SyntaxError]
+reparse :: String -> Maybe SyntaxError
+reparse = orFail . parse program ""
+
+program :: Reparser
 program = strip False
 
-strip :: Bool -> ParsecParser [SyntaxError]
-strip nested = fmap concat $ many (choice choices)
+strip :: Bool -> Reparser
+strip nested = fmap msum $ many (choice choices)
   where choices | nested = commonParsers
-                | otherwise = commonParsers ++ [unopen bracketMode, unopen parenMode, unopen braceMode]
+                | otherwise = commonParsers ++ withModes unopen
 
-        commonParsers = [stream, open bracketMode, open parenMode, open braceMode]
+        commonParsers = stream : withModes open
+        withModes f = map f [bracketMode, parenMode, braceMode]
 
-data Mode = Mode { l :: ParsecParser Char, r :: ParsecParser Char, uo :: SyntaxErrorKind, uc :: SyntaxErrorKind }
+data Mode = Mode {
+  leftParser :: ParsecParser Char,
+  rightParser :: ParsecParser Char,
+  unopenKind :: SyntaxErrorKind,
+  unclosedKind :: SyntaxErrorKind
+}
 
-parenMode = Mode lparen rparen UnopenParen UnclosedParen
+parenMode   = Mode lparen rparen UnopenParen UnclosedParen
 bracketMode = Mode lbracket rbracket UnopenBracket UnclosedBracket
-braceMode = Mode lbrace rbrace UnopenBrace UnclosedBrace
+braceMode   = Mode lbrace rbrace UnopenBrace UnclosedBrace
 
+open :: Mode -> Reparser
 open mode = do
-  pos <- getPosition
-  l mode
-  optionMaybe (strip True)
-  choice [end mode, unclosed mode pos]
+  position <- getPosition
+  nested <- startNesting mode
+  if isJust nested
+    then return nested
+    else endNesting mode position
 
-end mode = do
-  r mode
-  return []
-
+unopen :: Mode -> Reparser
 unopen mode = do
-  pos <- getPosition
-  r mode
-  return [SyntaxError (sourceLine pos) (sourceColumn pos) (uo mode)]
+  position <- getPosition
+  rightParser mode
+  returnSyntaxError position (unopenKind mode)
 
-unclosed mode pos = do
-  return [SyntaxError (sourceLine pos) (sourceColumn pos) (uc mode)]
+startNesting mode = leftParser mode *> (fmap join . optionMaybe $ strip True)
 
-stream = do
-  many1 letter
-  return []
+endNesting mode position = closed mode <|> unclosed mode position
+
+closed mode = rightParser mode *> return Nothing
+
+unclosed mode position = returnSyntaxError position (unclosedKind mode)
+
+stream :: Reparser
+stream = many1 (noneOf "{}[]()") *> return Nothing
+
+returnSyntaxError position = return . Just . SyntaxError (sourceLine position) (sourceColumn position)
 
 spec :: Spec
 spec = do
   describe "reparse" $ do
     it "`return[bar]`" $ do
-      reparse "return[bar]" `shouldBe` []
+      reparse "return[bar]" `shouldBe` Nothing
 
     it "``" $ do
-      reparse "" `shouldBe` []
+      reparse "" `shouldBe` Nothing
 
     it "`[]`" $ do
-      reparse "[]" `shouldBe` []
+      reparse "[]" `shouldBe` Nothing
 
     it "`[][]`" $ do
-      reparse "[][]" `shouldBe` []
+      reparse "[][]" `shouldBe` Nothing
 
     it "works with letters between brackets" $ do
-      reparse "[hello]" `shouldBe` []
+      reparse "[hello]" `shouldBe` Nothing
 
     it "works with uncloded bracket" $ do
-      reparse "[" `shouldBe` [SyntaxError 1 1 UnclosedBracket]
+      reparse "[" `shouldBe` Just (SyntaxError 1 1 UnclosedBracket)
 
     it "works with unclosed bracket and streams" $ do
-      reparse "[hello" `shouldBe` [SyntaxError 1 1 UnclosedBracket]
+      reparse "[hello" `shouldBe` Just (SyntaxError 1 1 UnclosedBracket)
 
     it "`return[bar`" $ do
-      reparse "return[bar" `shouldBe` [SyntaxError 1 7 UnclosedBracket]
+      reparse "return[bar" `shouldBe` Just (SyntaxError 1 7 UnclosedBracket)
 
     it "`[]]`" $ do
-      reparse "[]]" `shouldBe` [SyntaxError 1 3 UnopenBracket]
+      reparse "[]]" `shouldBe` Just (SyntaxError 1 3 UnopenBracket)
 
     it "works with many trivial unbalanced brackets, on first pair" $ do
-      reparse "[[]" `shouldBe` [SyntaxError 1 1 UnclosedBracket]
+      reparse "[[]" `shouldBe` Just (SyntaxError 1 1 UnclosedBracket)
 
     it "works with mixed streams and brackets" $ do
-      reparse "hello[world[hello]world" `shouldBe` [SyntaxError 1 6 UnclosedBracket]
+      reparse "hello[world[hello]world" `shouldBe` Just (SyntaxError 1 6 UnclosedBracket)
 
     it "works with nested streams and brackets" $ do
-      reparse "hello[world[hello]world]" `shouldBe` []
+      reparse "hello[world[hello]world]" `shouldBe` Nothing
 
     it "`hello[world[hello[world]]`" $ do
-      reparse "hello[world[hello[world]]" `shouldBe` [SyntaxError 1 6 UnclosedBracket]
+      reparse "hello[world[hello[world]]" `shouldBe` Just (SyntaxError 1 6 UnclosedBracket)
 
     it "`hello[world]hello[world]`" $ do
-      reparse "hello[world]hello[world]" `shouldBe` []
+      reparse "hello[world]hello[world]" `shouldBe` Nothing
 
     it "`return(bar)`" $ do
-      reparse "return(bar)" `shouldBe` []
+      reparse "return(bar)" `shouldBe` Nothing
 
     it "`()`" $ do
-      reparse "()" `shouldBe` []
+      reparse "()" `shouldBe` Nothing
 
     it "`()()`" $ do
-      reparse "()()" `shouldBe` []
+      reparse "()()" `shouldBe` Nothing
 
     it "works with letters between brackets" $ do
-      reparse "(hello)" `shouldBe` []
+      reparse "(hello)" `shouldBe` Nothing
 
     it "(" $ do
-      reparse "(" `shouldBe` [SyntaxError 1 1 UnclosedParen]
+      reparse "(" `shouldBe` Just (SyntaxError 1 1 UnclosedParen)
 
     it "works with unclosed bracket and streams" $ do
-      reparse "(hello" `shouldBe` [SyntaxError 1 1 UnclosedParen]
+      reparse "(hello" `shouldBe` Just (SyntaxError 1 1 UnclosedParen)
 
     it "`return(bar`" $ do
-      reparse "return(bar" `shouldBe` [SyntaxError 1 7 UnclosedParen]
+      reparse "return(bar" `shouldBe` Just (SyntaxError 1 7 UnclosedParen)
 
     it "`)`" $ do
-      reparse ")" `shouldBe` [SyntaxError 1 1 UnopenParen]
+      reparse ")" `shouldBe` Just (SyntaxError 1 1 UnopenParen)
 
     it "works with many trivial unbalanced brackets, on first pair" $ do
-      reparse "(()" `shouldBe` [SyntaxError 1 1 UnclosedParen]
+      reparse "(()" `shouldBe` Just (SyntaxError 1 1 UnclosedParen)
 
     it "works with mixed streams and brackets" $ do
-      reparse "hello(world(hello)world" `shouldBe` [SyntaxError 1 6 UnclosedParen]
+      reparse "hello(world(hello)world" `shouldBe` Just (SyntaxError 1 6 UnclosedParen)
 
     it "works with nested streams and brackets" $ do
-      reparse "hello(world(hello)world)" `shouldBe` []
+      reparse "hello(world(hello)world)" `shouldBe` Nothing
 
     it "`hello(world(hello(world)`" $ do
-      reparse "hello(world(hello(world)" `shouldBe` [SyntaxError 1 6 UnclosedParen]
+      reparse "hello(world(hello(world)" `shouldBe` Just (SyntaxError 1 12 UnclosedParen)
+
+    it "`hello(world(hello(world`" $ do
+      reparse "hello(world(hello(world" `shouldBe` Just (SyntaxError 1 18 UnclosedParen)
 
     it "`hello(world)hello(world)`" $ do
-      reparse "hello(world)hello(world)" `shouldBe` []
+      reparse "hello(world)hello(world)" `shouldBe` Nothing
 
     it "`([])`" $ do
-      reparse "([])" `shouldBe` []
+      reparse "([])" `shouldBe` Nothing
 
     it "`{[(hello)]}`" $ do
-      reparse "{[(hello)]}" `shouldBe` []
+      reparse "{[(hello)]}" `shouldBe` Nothing
 
     it "`(]`" $ do
-      reparse "(]" `shouldBe` [SyntaxError 1 1 UnclosedParen, SyntaxError 1 2 UnopenBracket]
+      reparse "(]" `shouldBe` Just (SyntaxError 1 1 UnclosedParen)
+
+    it "`{ ( { ( []{} ) } }`" $ do
+      reparse "{ ( { ( []{} ) } }" `shouldBe` Just (SyntaxError 1 3 UnclosedParen)
+
+    it "`{ ( { ( ) } }`" $ do
+      reparse "{ ( { ( ) } }" `shouldBe` Just (SyntaxError 1 3 UnclosedParen)
+
+    it "`{ ( { } }`" $ do
+      reparse "{ ( { } }" `shouldBe` Just (SyntaxError 1 3 UnclosedParen)
+
+    it "`{(}`" $ do
+      reparse "{(}" `shouldBe` Just (SyntaxError 1 2 UnclosedParen)
+
+    it "`{[}`" $ do
+      reparse "{[}" `shouldBe` Just (SyntaxError 1 2 UnclosedBracket)
+
+    it "`[{]`" $ do
+      reparse "[{]" `shouldBe` Just (SyntaxError 1 2 UnclosedBrace)
+
+    it "`[{`" $ do
+      reparse "[{" `shouldBe` Just (SyntaxError 1 2 UnclosedBrace)
+
+    it "`[}]`" $ do
+      reparse "[}]" `shouldBe` Just (SyntaxError 1 1 UnclosedBracket)
+
+    it "`( ( )`" $ do
+      reparse "( ( )" `shouldBe` Just (SyntaxError 1 1 UnclosedParen)
+
+    it "`public class Main {\npublic static void main(String[] args) {\nSystem.out.println(new int[]{1, 2});\n}\n}`" $ do
+      reparse "public class Main {\npublic static void main(String[] args) {\nSystem.out.println(new int[]{1, 2});\n}\n}" `shouldBe` Nothing
+
+    it "`public class Main {\npublic static void main(String[] args) \nSystem.out.println(new int[]{1, 2});\n}\n}`" $ do
+      reparse "public class Main {\npublic static void main(String[] args) \nSystem.out.println(new int[]{1, 2});\n}\n}" `shouldBe` Just (SyntaxError 5 1 UnopenBrace)
+
+    it "`public class Main {\npublic static void main(String[] args) {\nSystem.out.println(new int[]{1, 2});\n}`" $ do
+      reparse "public class Main {\npublic static void main(String[] args) {\nSystem.out.println(new int[]{1, 2});\n}" `shouldBe` Just (SyntaxError 1 19 UnclosedBrace)
