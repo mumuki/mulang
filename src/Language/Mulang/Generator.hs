145fa5382817669c9133a2fe97c070e4d38063e8
Julian Berbel Alt <julianberbel@hotmail.com>
2017-12-22 12:48:51 -0300

    change try subexpressions order to follow declared one

--- src/Language/Mulang/Generator.hs
+++ src/Language/Mulang/Generator.hs
@@ -74,7 +74,7 @@ expressions expr = expr : concatMap expressions (subExpressions expr)
     subExpressions (MuObject es)           = [es]
     subExpressions (MuTuple as)            = as
     subExpressions (MuList as)             = as
-    subExpressions (Try t cs f)            = [t, f] ++ map snd cs
+    subExpressions (Try t cs f)            = t : map snd cs ++ [f]
     subExpressions _                       = []
 
 
