d181fae3319fa714c60441165668cabfba25d5c8
Franco Bulgarelli <franco@mumuki.org>
2017-12-25 15:32:02 -0300

    Traversing New expressions

--- src/Language/Mulang/Generator.hs
+++ src/Language/Mulang/Generator.hs
@@ -66,6 +66,7 @@ expressions expr = expr : concatMap expressions (subExpressions expr)
     subExpressions (MuList as)             = as
     subExpressions (MuObject es)           = [es]
     subExpressions (MuTuple as)            = as
+    subExpressions (New _ es)              = es
     subExpressions (Not e)                 = [e]
     subExpressions (Object _ v)            = [v]
     subExpressions (Repeat e1 e2)          = [e1, e2]
