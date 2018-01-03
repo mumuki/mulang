291aeb8e00b752a9779cf6e9651ef3349b1ce716
Franco Bulgarelli <franco@mumuki.org>
2018-01-03 15:14:18 -0300

    Welcome 3.6.0!

--- app/Version.hs
+++ app/Version.hs
@@ -6,7 +6,7 @@ prettyVersion :: String
 prettyVersion = "Mulang Release " ++ version ++ ", compiled on " ++ compilationTimestamp
 
 version :: String
-version = "3.5.2"
+version = "3.6.0"
 
 compilationTimestamp :: String
 compilationTimestamp = __DATE__ ++ " " ++ __TIME__
