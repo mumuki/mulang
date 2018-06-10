5c0e65e96ccf56bca257282650adc300f0bd1ec4
Franco Bulgarelli <franco@mumuki.org>
2018-06-10 15:12:33 -0300

    Adapting main to js env

--- app/Main.hs
+++ app/Main.hs
@@ -1,58 +1,26 @@
-{-# LANGUAGE DeriveGeneric, QuasiQuotes, OverloadedStrings #-}
+module Main (main, analyseIO) where
 
-module Main (main) where
+import Data.JSString (JSString)
+import Data.JSString as S (pack, unpack)
 
 import Language.Mulang.Analyzer (analyse)
 import Language.Mulang.Analyzer.Analysis.Json ()
 import Data.Aeson (eitherDecode, encode)
-import System.Environment (getArgs)
 import Control.Fallible (orFail)
 
-import           Data.List (intercalate)
-import           Version (prettyVersion)
 import           Data.Text.Lazy.Encoding (encodeUtf8)
-import qualified Data.ByteString.Lazy as LBS (putStrLn)
 import qualified Data.Text.Lazy as T (pack)
-import           Data.Text (unpack)
-import           NeatInterpolation (text)
 
+import qualified Data.ByteString.Lazy.Char8 as B (unpack)
 
 main :: IO ()
-main = do
-  argsBody <- fmap parseArgs $ getArgs
-  streamBody <- getContents
-  let body = if argsBody == "-s" then streamBody else argsBody
-  result <- analyseJson body
-  LBS.putStrLn result
+main = return ()
 
-analyseJson = fmap encode . analyse . decode . encodeUtf8 . T.pack
+analyseIO :: JSString -> IO JSString
+analyseIO body = fmap  S.pack . analyseJson . S.unpack $ body
+  where
 
-parseArgs :: [String] -> String
-parseArgs [jsonBody] = jsonBody
-parseArgs _          = error (intercalate "\n" [prettyVersion, usage])
+    analyseJson :: String -> IO String
+    analyseJson = fmap (B.unpack . encode) . analyse . decode . encodeUtf8 . T.pack
 
-decode = orFail . eitherDecode
-
-usage = unpack [text|
-        Wrong usage.
-
-         #read from STDIN
-         $ mulang -s
-
-         #read from argument
-         $ mulang '{
-             "sample" : {
-                "tag" : "CodeSample",
-                "language" : "Haskell",
-                "content" : "x = 1"
-             },
-             "spec" : {
-                "expectations" : [
-                   {
-                      "binding" : ":Intransitive:x",
-                      "inspection" : "Uses:*"
-                   }
-                ],
-                "smellsSet" : { "tag" : "NoSmells" }
-             }
-          }'|]
+    decode = orFail . eitherDecode
