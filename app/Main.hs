238c850a90347a0a4ee520c9ac06d6d55a83366b
Franco Bulgarelli <franco@mumuki.org>
2018-06-10 21:33:19 -0300

    Restoring original stack, cabal and Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,58 @@
-module Main (main, analyseIO) where
+{-# LANGUAGE DeriveGeneric, QuasiQuotes, OverloadedStrings #-}
 
-import Data.JSString (JSString)
-import Data.JSString as S (pack, unpack)
+module Main (main) where
 
 import Language.Mulang.Analyzer (analyse)
 import Language.Mulang.Analyzer.Analysis.Json ()
 import Data.Aeson (eitherDecode, encode)
+import System.Environment (getArgs)
 import Control.Fallible (orFail)
 
+import           Data.List (intercalate)
+import           Version (prettyVersion)
 import           Data.Text.Lazy.Encoding (encodeUtf8)
+import qualified Data.ByteString.Lazy as LBS (putStrLn)
 import qualified Data.Text.Lazy as T (pack)
+import           Data.Text (unpack)
+import           NeatInterpolation (text)
 
-import qualified Data.ByteString.Lazy.Char8 as B (unpack)
 
 main :: IO ()
-main = return ()
+main = do
+  argsBody <- fmap parseArgs $ getArgs
+  streamBody <- getContents
+  let body = if argsBody == "-s" then streamBody else argsBody
+  result <- analyseJson body
+  LBS.putStrLn result
 
-analyseIO :: JSString -> IO JSString
-analyseIO body = fmap  S.pack . analyseJson . S.unpack $ body
-  where
+analyseJson = fmap encode . analyse . decode . encodeUtf8 . T.pack
 
-    analyseJson :: String -> IO String
-    analyseJson = fmap (B.unpack . encode) . analyse . decode . encodeUtf8 . T.pack
+parseArgs :: [String] -> String
+parseArgs [jsonBody] = jsonBody
+parseArgs _          = error (intercalate "\n" [prettyVersion, usage])
 
-    decode = orFail . eitherDecode
+decode = orFail . eitherDecode
+
+usage = unpack [text|
+        Wrong usage.
+
+         #read from STDIN
+         $ mulang -s
+
+         #read from argument
+         $ mulang '{
+             "sample" : {
+                "tag" : "CodeSample",
+                "language" : "Haskell",
+                "content" : "x = 1"
+             },
+             "spec" : {
+                "expectations" : [
+                   {
+                      "binding" : ":Intransitive:x",
+                      "inspection" : "Uses:*"
+                   }
+                ],
+                "smellsSet" : { "tag" : "NoSmells" }
+             }
+          }'|]
