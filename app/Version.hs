944a2c56f9e93c5f09ad933cff0b037cf87079b9
Julián Berbel Alt <julianberbel@hotmail.com>
2018-07-24 13:58:49 -0300

    Welcome 4.2.0!

--- app/Version.hs
+++ app/Version.hs
@@ -6,7 +6,7 @@ prettyVersion :: String
 prettyVersion = "Mulang Release " ++ version ++ ", compiled on " ++ compilationTimestamp
 
 version :: String
-version = "4.1.0"
+version = "4.2.0"
 
 compilationTimestamp :: String
 compilationTimestamp = __DATE__ ++ " " ++ __TIME__
--- gem/lib/mulang/version.rb
+++ gem/lib/mulang/version.rb
@@ -1,4 +1,4 @@
 module Mulang
-  VERSION = "4.1.0"
-  MULANG_VERSION = "4.1.0"
+  VERSION = "4.2.0"
+  MULANG_VERSION = "4.2.0"
 end
