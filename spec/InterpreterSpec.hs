bd7fc37082b600dd62aeb91bf1ab5b6bff4c2af4
Julián Berbel Alt <julianberbel@hotmail.com>
2018-12-14 12:12:20 -0300

    add python interpreter and runner tests

--- spec/InterpreterSpec.hs
+++ spec/InterpreterSpec.hs
@@ -6,90 +6,160 @@ import           Test.Hspec
 import           Interpreter.Mulang
 import qualified Data.Map.Strict as Map
 import           Language.Mulang.Parsers.JavaScript
+import           Language.Mulang.Parsers.Python
 
 import           Data.Text (Text, unpack)
 import           NeatInterpolation (text)
 
 lastRef result = (\(ref, ctx) -> (Map.! ref) . globalObjects $ ctx) <$> result
 
-run code = eval defaultContext  . js . unpack $ code
+run language code = eval defaultContext  . language . unpack $ code
+runjs = run js
+runpy = run py
 
 spec :: Spec
 spec = do
   describe "evalExpr" $ do
-    it "evals addition" $ do
-      lastRef (run "1 + 2") `shouldReturn` MuNumber 3
+    context "javascript" $ do
+      it "evals addition" $ do
+        lastRef (runjs "1 + 2") `shouldReturn` MuNumber 3
 
-    it "evals subtraction" $ do
-      lastRef (run "2 - 1") `shouldReturn` MuNumber 1
+      it "evals subtraction" $ do
+        lastRef (runjs "2 - 1") `shouldReturn` MuNumber 1
 
-    it "evals multiplication" $ do
-      lastRef (run "2 * 3") `shouldReturn` MuNumber 6
+      it "evals multiplication" $ do
+        lastRef (runjs "2 * 3") `shouldReturn` MuNumber 6
 
-    it "evals mod" $ do
-      lastRef (run "7 % 4") `shouldReturn` MuNumber 3
+      it "evals mod" $ do
+        lastRef (runjs "7 % 4") `shouldReturn` MuNumber 3
 
-    it "evals and" $ do
-      lastRef (run "true && true") `shouldReturn` MuBool True
+      it "evals and" $ do
+        lastRef (runjs "true && true") `shouldReturn` MuBool True
 
-    it "evals or" $ do
-      lastRef (run "false || false") `shouldReturn` MuBool False
+      it "evals or" $ do
+        lastRef (runjs "false || false") `shouldReturn` MuBool False
 
-    it "evals comparison" $ do
-      lastRef (run "7 > 4") `shouldReturn` MuBool True
+      it "evals not" $ do
+        lastRef (runjs "!false") `shouldReturn` MuBool True
 
-    context "evals equal" $ do
-      it "is false when values are different" $ do
-        lastRef (run "123 == 321") `shouldReturn` MuBool False
+      it "evals comparison" $ do
+        lastRef (runjs "7 > 4") `shouldReturn` MuBool True
 
-      it "is true when values are the same" $ do
-        lastRef (run "'123' == '123'") `shouldReturn` MuBool True
+      context "evals equal" $ do
+        it "is false when values are different" $ do
+          lastRef (runjs "123 == 321") `shouldReturn` MuBool False
 
-      it "is false when values are of different types" $ do
-        lastRef (run "'123' == 123") `shouldReturn` MuBool False
+        it "is true when values are the same" $ do
+          lastRef (runjs "'123' == '123'") `shouldReturn` MuBool True
 
-    context "evals if" $ do
-      it "condition is true then evals first branch" $ do
-        lastRef (run [text|
-          if(true){
-            123
-          } else {
-            456
-          }|]) `shouldReturn` MuNumber 123
+        it "is false when values are of different types" $ do
+          lastRef (runjs "'123' == 123") `shouldReturn` MuBool False
 
-      it "condition is false then evals second branch" $ do
-        lastRef (run [text|
-          if(false){
-            123
-          } else {
-            456
-          }|]) `shouldReturn` MuNumber 456
+      context "evals if" $ do
+        it "condition is true then evals first branch" $ do
+          lastRef (runjs [text|
+            if(true){
+              123
+            } else {
+              456
+            }|]) `shouldReturn` MuNumber 123
 
-    it "evals functions" $ do
-      lastRef (run [text|
-        function a() {
-          return 123;
-        }
-        a()|]) `shouldReturn` MuNumber 123
+        it "condition is false then evals second branch" $ do
+          lastRef (runjs [text|
+            if(false){
+              123
+            } else {
+              456
+            }|]) `shouldReturn` MuNumber 456
 
-    it "handles scopes" $ do
-      lastRef (run [text|
-        function a() {
-          function b(){}
-        }
-        b()|]) `shouldThrow` (errorCall "Exception thrown outside try: MuString \"Reference not found for name 'b'\"")
+      it "evals functions" $ do
+        lastRef (runjs [text|
+          function a() {
+            return 123;
+          }
+          a()|]) `shouldReturn` MuNumber 123
 
-    it "handles whiles" $ do
-      lastRef (run [text|
-        var a = 0;
+      it "handles scopes" $ do
+        lastRef (runjs [text|
+          function a() {
+            function b(){}
+          }
+          b()|]) `shouldThrow` (errorCall "Exception thrown outside try: MuString \"Reference not found for name 'b'\"")
 
-        while(a < 10) a = a + 1;
-        a;|]) `shouldReturn` MuNumber 10
+      it "handles whiles" $ do
+        lastRef (runjs [text|
+          var a = 0;
 
-    it "handles fors" $ do
-      lastRef (run [text|
-        var a = 0;
-        var i = 0;
+          while(a < 10) a = a + 1;
+          a;|]) `shouldReturn` MuNumber 10
 
-        for(i = 0; i <= 10; i++) a = a + i;
-        a;|]) `shouldReturn` MuNumber 55
+      it "handles fors" $ do
+        lastRef (runjs [text|
+          var a = 0;
+          var i = 0;
+
+          for(i = 0; i <= 10; i++) a = a + i;
+          a;|]) `shouldReturn` MuNumber 55
+
+
+    context "python" $ do
+      it "evals addition" $ do
+        lastRef (runpy "1 + 2") `shouldReturn` MuNumber 3
+
+      it "evals subtraction" $ do
+        lastRef (runpy "2 - 1") `shouldReturn` MuNumber 1
+
+      it "evals multiplication" $ do
+        lastRef (runpy "2 * 3") `shouldReturn` MuNumber 6
+
+      it "evals mod" $ do
+        lastRef (runpy "7 % 4") `shouldReturn` MuNumber 3
+
+      it "evals and" $ do
+        --lastRef (runpy "true && true") `shouldReturn` MuBool True
+        pending
+
+      it "evals or" $ do
+        --lastRef (runpy "false || false") `shouldReturn` MuBool False
+        pending
+
+      it "evals comparison" $ do
+        lastRef (runpy "7 > 4") `shouldReturn` MuBool True
+
+      context "evals equal" $ do
+        it "is false when values are different" $ do
+          lastRef (runpy "123 == 321") `shouldReturn` MuBool False
+
+        it "is true when values are the same" $ do
+          lastRef (runpy "'123' == '123'") `shouldReturn` MuBool True
+
+        it "is false when values are of different types" $ do
+          lastRef (runpy "'123' == 123") `shouldReturn` MuBool False
+
+      context "evals if" $ do
+        it "condition is true then evals first branch" $ do
+          lastRef (runpy [text|
+            if True: 123
+            else: 456|]) `shouldReturn` MuNumber 123
+
+        it "condition is false then evals second branch" $ do
+          lastRef (runpy [text|
+            if False: 123
+            else: 456|]) `shouldReturn` MuNumber 456
+
+      it "evals functions" $ do
+        lastRef (runpy [text|
+          def a():
+            return 123
+          a()|]) `shouldReturn` MuNumber 123
+
+      it "handles whiles" $ do
+        lastRef (runpy [text|
+          a = 0
+
+          while(a < 10): a = a + 1
+          a|]) `shouldReturn` MuNumber 10
+
+      it "evals not" $ do
+        --lastRef (runpy "not false") `shouldReturn` MuBool True
+        pending
--- spec/RunnerSpec.hs
+++ spec/RunnerSpec.hs
@@ -4,107 +4,186 @@ module RunnerSpec (spec) where
 
 import           Test.Hspec
 import           Language.Mulang.Parsers.JavaScript
+import           Language.Mulang.Parsers.Python
 import           Interpreter.Runner
 import           Interpreter.Mulang.Tests
 
 import           Data.Text (Text, unpack)
 import           NeatInterpolation (text)
 
-parse = js . unpack
-run code testSuite = runTests (parse code) (getTests $ parse testSuite)
+parse language = language . unpack
+run language code testSuite = runTests (parse language code) (getTests $ parse language testSuite)
+runjs = run js
+runpy = run py
 
 spec :: Spec
 spec = do
   describe "runTests" $ do
-    it "runs tests" $ do
-      let code = [text||]
-      let suite = [text|
-        it("is true", function() {
-          assert(true)
-        })
-      |]
-      run code suite `shouldReturn` [(["is true"], Success)]
-
-    context "assert.equals" $ do
-      it "passes if values are equal" $ do
+    context "javascript" $ do
+      it "runs tests" $ do
         let code = [text||]
         let suite = [text|
-          it("passes", function() {
-            assert.equals(1, 1)
+          it("is true", function() {
+            assert(true)
           })
         |]
-        run code suite `shouldReturn` [(["passes"], Success)]
+        runjs code suite `shouldReturn` [(["is true"], Success)]
 
-      it "fails if values are not equal" $ do
+      context "assert.equals" $ do
+        it "passes if values are equal" $ do
+          let code = [text||]
+          let suite = [text|
+            it("passes", function() {
+              assert.equals(1, 1)
+            })
+          |]
+          runjs code suite `shouldReturn` [(["passes"], Success)]
+
+        it "fails if values are not equal" $ do
+          let code = [text||]
+          let suite = [text|
+            it("fails", function() {
+              assert.equals(1, 2)
+            })
+          |]
+          runjs code suite `shouldReturn` [(["fails"], Failure "MuString \"Expected MuNumber 1.0 but got: MuNumber 2.0\"")]
+
+      it "can handle failed tests" $ do
         let code = [text||]
         let suite = [text|
           it("fails", function() {
-            assert.equals(1, 2)
+            assert(false)
           })
         |]
-        run code suite `shouldReturn` [(["fails"], Failure "MuString \"Expected MuNumber 1.0 but got: MuNumber 2.0\"")]
+        runjs code suite `shouldReturn` [(["fails"], Failure "MuString \"Expected True but got: False\"")]
 
-    it "can handle failed tests" $ do
-      let code = [text||]
-      let suite = [text|
-        it("fails", function() {
-          assert(false)
-        })
-      |]
-      run code suite `shouldReturn` [(["fails"], Failure "MuString \"Expected True but got: False\"")]
-
-    it "can handle errored tests" $ do
-      let code = [text||]
-      let suite = [text|
-        it("errors", function() {
-          assert.equals(succ(3), 4)
-        })
-      |]
-      run code suite `shouldReturn` [(["errors"], Failure "MuString \"Reference not found for name 'succ'\"")]
-
-    it "can reference functions defined in code" $ do
-      let code = [text|
-        function succ(n) {
-          return n + 1;
-        }
-      |]
-      let suite = [text|
-        it("succ increments a given number by 1", function() {
-          assert.equals(succ(3), 4)
-        })
-      |]
-      run code suite `shouldReturn` [(["succ increments a given number by 1"], Success)]
-
-    it "accepts describes" $ do
-      let code = [text|
-        function succ(n) {
-          return n + 1;
-        }
-      |]
-      let suite = [text|
-        describe("succ", function () {
-          it("increments a given number by 1", function() {
+      it "can handle errored tests" $ do
+        let code = [text||]
+        let suite = [text|
+          it("errors", function() {
             assert.equals(succ(3), 4)
           })
-        })
-      |]
-      run code suite `shouldReturn` [(["succ", "increments a given number by 1"], Success)]
+        |]
+        runjs code suite `shouldReturn` [(["errors"], Failure "MuString \"Reference not found for name 'succ'\"")]
 
-    it "accepts multiple test cases" $ do
-      let code = [text|
-        function succ(n) {
-          return n + 1;
-        }
-      |]
-      let suite = [text|
-        it("if I pass a 3 to succ it returns 4", function() {
-          assert.equals(succ(3), 4)
-        })
+      it "can reference functions defined in code" $ do
+        let code = [text|
+          function succ(n) {
+            return n + 1;
+          }
+        |]
+        let suite = [text|
+          it("succ increments a given number by 1", function() {
+            assert.equals(succ(3), 4)
+          })
+        |]
+        runjs code suite `shouldReturn` [(["succ increments a given number by 1"], Success)]
 
-        it("if I pass a 10 to succ it returns 11", function() {
-          assert.equals(succ(10), 11)
-        })
-      |]
-      run code suite `shouldReturn` [(["if I pass a 3 to succ it returns 4"], Success),
-                                     (["if I pass a 10 to succ it returns 11"], Success)]
+      it "accepts describes" $ do
+        let code = [text|
+          function succ(n) {
+            return n + 1;
+          }
+        |]
+        let suite = [text|
+          describe("succ", function () {
+            it("increments a given number by 1", function() {
+              assert.equals(succ(3), 4)
+            })
+          })
+        |]
+        runjs code suite `shouldReturn` [(["succ", "increments a given number by 1"], Success)]
 
+      it "accepts multiple test cases" $ do
+        let code = [text|
+          function succ(n) {
+            return n + 1;
+          }
+        |]
+        let suite = [text|
+          it("if I pass a 3 to succ it returns 4", function() {
+            assert.equals(succ(3), 4)
+          })
+
+          it("if I pass a 10 to succ it returns 11", function() {
+            assert.equals(succ(10), 11)
+          })
+        |]
+        runjs code suite `shouldReturn` [ (["if I pass a 3 to succ it returns 4"], Success),
+                                          (["if I pass a 10 to succ it returns 11"], Success)]
+
+
+    context "python" $ do
+      it "runs tests" $ do
+        let code = [text||]
+        let suite = [text|
+          class TestPython(unittest.TestCase):
+            def test_is_true():
+              self.assertTrue(True)
+        |]
+        runpy code suite `shouldReturn` [(["TestPython", "test_is_true"], Success)]
+
+      context "assert.equals" $ do
+        it "passes if values are equal" $ do
+          let code = [text||]
+          let suite = [text|
+            class TestPython(unittest.TestCase):
+              def test_passes():
+                self.assertEqual(1, 1)
+          |]
+          runpy code suite `shouldReturn` [(["TestPython", "test_passes"], Success)]
+
+        it "fails if values are not equal" $ do
+          let code = [text||]
+          let suite = [text|
+            class TestPython(unittest.TestCase):
+              def test_fails():
+                self.assertEqual(1, 2)
+          |]
+          runpy code suite `shouldReturn` [(["TestPython", "test_fails"], Failure "MuString \"Expected MuNumber 1.0 but got: MuNumber 2.0\"")]
+
+      it "can handle failed tests" $ do
+        let code = [text||]
+        let suite = [text|
+          class TestPython(unittest.TestCase):
+            def test_fails():
+              self.assertTrue(False)
+        |]
+        runpy code suite `shouldReturn` [(["TestPython", "test_fails"], Failure "MuString \"Expected True but got: False\"")]
+
+      it "can handle errored tests" $ do
+        let code = [text||]
+        let suite = [text|
+          class TestPython(unittest.TestCase):
+            def test_errors():
+              self.assertEqual(succ(3), 4)
+        |]
+        runpy code suite `shouldReturn` [(["TestPython", "test_errors"], Failure "MuString \"Reference not found for name 'succ'\"")]
+
+      it "can reference functions defined in code" $ do
+        let code = [text|
+          def succ(n):
+            return n + 1
+        |]
+        let suite = [text|
+          class TestPython(unittest.TestCase):
+            def test_succ_increments_a_given_numer_by_1():
+              self.assertEqual(succ(3), 4)
+        |]
+        runpy code suite `shouldReturn` [(["TestPython", "test_succ_increments_a_given_numer_by_1"], Success)]
+
+      it "accepts multiple test cases" $ do
+        let code = [text|
+          def succ(n):
+            return n + 1
+        |]
+        let suite = [text|
+          class TestSucc(unittest.TestCase):
+            def test_if_I_pass_a_3_to_succ_it_returns_4():
+              self.assertEqual(succ(3), 4)
+
+            def test_if_I_pass_a_10_to_succ_it_returns_11():
+              self.assertEqual(succ(10), 11)
+        |]
+        runpy code suite `shouldReturn` [ (["TestSucc", "test_if_I_pass_a_3_to_succ_it_returns_4"], Success),
+                                          (["TestSucc", "test_if_I_pass_a_10_to_succ_it_returns_11"], Success)]
