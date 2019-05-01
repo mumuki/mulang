12a26cbbd4b3b7793f79197fad58a70b6391bdd5
Franco Bulgarelli <franco@mumuki.org>
2019-05-01 19:56:52 -0300

    Refactor to autocorrector

--- src/Language/Mulang/Analyzer/Autocorrector.hs
+++ src/Language/Mulang/Analyzer/Autocorrector.hs
@@ -2,9 +2,13 @@ module Language.Mulang.Analyzer.Autocorrector (autocorrect) where
 
 import           Language.Mulang.Analyzer.Analysis
 
-import           Language.Mulang.Operators (Token)
+import           Language.Mulang.Operators (Token, TokensTable)
+import           Language.Mulang.Operators.Haskell (haskellTokensTable)
+import           Language.Mulang.Operators.Ruby (rubyTokensTable)
+import           Language.Mulang.Operators.Java (javaTokensTable)
+import           Language.Mulang.Operators.Python (pythonTokensTable)
 
-import           Data.Maybe (fromMaybe)
+import           Data.Maybe (fromMaybe, fromJust)
 import qualified Data.Map.Strict as Map
 
 
@@ -18,14 +22,14 @@ import qualified Data.Map.Strict as Map
 --  4. fills the domainLanguage rules when it is not present but can be inferred from the originalLanguage
 --  5. fills the domainLanguage's caseStyle when it is not present but can be inferred from the originalLanguage
 autocorrect :: Analysis -> Analysis
-autocorrect (Analysis f s@(AnalysisSpec { originalLanguage = Just l })) = Analysis f (autocorrectSpec l s)
+autocorrect (Analysis f s@(AnalysisSpec { originalLanguage = Just _ })) = Analysis f (autocorrectSpec s)
 autocorrect (Analysis f@(CodeSample { language = l } ) s)               = autocorrect (Analysis f s { originalLanguage = Just l }) -- (1)
 autocorrect a                                                           = a
 
-autocorrectSpec :: Language -> AnalysisSpec -> AnalysisSpec
-autocorrectSpec l s = runFixes [rulesFix, expectationsFix, emptyDomainLanguageFix, domainLanguageCaseStyleFix]
+autocorrectSpec :: AnalysisSpec -> AnalysisSpec
+autocorrectSpec s = runFixes [rulesFix, expectationsFix, emptyDomainLanguageFix, domainLanguageCaseStyleFix]
   where
-    runFixes = foldl combine s . map ($l)
+    runFixes = foldl combine s . map ($ (justOriginalLanguage s))
     combine s f | Just s' <- f s = s'
                 | otherwise = s
 
@@ -39,12 +43,15 @@ rulesFix l s = do
   return s { autocorrectionRules = Just (inferAutocorrectionRules l) }
 
 expectationsFix :: Fix -- (3)
-expectationsFix l s = do
+expectationsFix _ s = do
   AnalysisSpec { expectations = Just es } <- Just s
-  return s { expectations = Just (autocorrectExpectations l es) }
+  return s { expectations = Just (map autocorrectExpectation es) }
+  where
+    autocorrectExpectation :: Expectation -> Expectation
+    autocorrectExpectation (Expectation b i) = Expectation b . fromMaybe i . Map.lookup i . justAutocorrectionRules $ s
 
 emptyDomainLanguageFix :: Fix  -- (4)
-emptyDomainLanguageFix l s = do
+emptyDomainLanguageFix _ s = do
   AnalysisSpec { domainLanguage = Nothing } <- Just s
   return s { domainLanguage = Just emptyDomainLanguage }
 
@@ -54,55 +61,59 @@ domainLanguageCaseStyleFix l s = do
   DomainLanguage { caseStyle = Nothing } <- Just dl
   return s { domainLanguage = Just (dl { caseStyle = Just (inferCaseStyle l) }) }
 
+-- Inferences
 
--- Misc
+type Inference a = Language -> a
 
-autocorrectExpectations :: Language -> [Expectation] -> [Expectation]
-autocorrectExpectations l = map (autocorrectExpectation l)
+inferTokensTable :: Inference TokensTable
+inferTokensTable Haskell = haskellTokensTable
+inferTokensTable Java    = javaTokensTable
+inferTokensTable Ruby    = rubyTokensTable
+inferTokensTable Python  = pythonTokensTable
+inferTokensTable _       = Map.empty
 
-autocorrectExpectation :: Language -> Expectation -> Expectation
-autocorrectExpectation l (Expectation b i) = Expectation b . fromMaybe i . Map.lookup i . inferAutocorrectionRules $ l
-
-inferCaseStyle :: Language -> CaseStyle
+inferCaseStyle :: Inference CaseStyle
 inferCaseStyle Python  = RubyCase
 inferCaseStyle Python2 = RubyCase
 inferCaseStyle Python3 = RubyCase
 inferCaseStyle Ruby    = RubyCase
 inferCaseStyle _       = CamelCase
 
-buildAutocorrectorRules :: [(Token, Inspection)] -> AutocorrectionRules
-buildAutocorrectorRules = Map.fromList . concatMap encodeEntry
+inferAutocorrectionRules :: Inference AutocorrectionRules
+inferAutocorrectionRules = buildAutocorrectorRules . inferAutocorrectionRules'
   where
+    buildAutocorrectorRules :: [(Token, Inspection)] -> AutocorrectionRules
+    buildAutocorrectorRules = Map.fromList . concatMap encodeEntry
+
     encodeEntry :: (Token, Inspection) -> [(Inspection, Inspection)]
     encodeEntry (token, inspection) = map (\e -> (e token, inspection)) [("Uses:" ++), ("Declares:"++)]
 
-inferAutocorrectionRules :: Language -> AutocorrectionRules
-inferAutocorrectionRules Haskell =
-  buildAutocorrectorRules [
+inferAutocorrectionRules' Haskell = [
     ("type", "DeclaresTypeAlias"),
     ("if", "UsesIf")
   ]
-inferAutocorrectionRules Java =
-  buildAutocorrectorRules [
+inferAutocorrectionRules' Java = [
     ("if", "UsesIf"),
     ("class", "DeclaresClass"),
     ("interface", "DeclaresInterface"),
     ("for", "UsesForLoop")
   ]
-inferAutocorrectionRules Ruby =
-  buildAutocorrectorRules [
+inferAutocorrectionRules' Ruby = [
     ("if", "UsesIf"),
     ("class", "DeclaresClass"),
     ("def", "DeclaresComputation"),
     ("for", "UsesForeach"),
     ("include",  "Includes")
   ]
-inferAutocorrectionRules Python =
-  buildAutocorrectorRules [
+inferAutocorrectionRules' Python = [
     ("if", "UsesIf"),
     ("class", "DeclaresClass"),
     ("def", "DeclaresComputation"),
     ("for", "UsesForeach")
   ]
-inferAutocorrectionRules _ = Map.empty
+inferAutocorrectionRules' _ = []
 
+-- Misc
+
+justOriginalLanguage = fromJust . originalLanguage
+justAutocorrectionRules = fromJust . autocorrectionRules
