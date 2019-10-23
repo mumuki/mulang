14be530a5427f9cfbac4c35b9a68cf6e435e8b62
Franco Bulgarelli <franco@mumuki.org>
2019-10-22 23:25:16 -0300

    Including mulangjs gem in release script

--- release.sh
+++ release.sh
@@ -13,11 +13,13 @@ fi
 
 echo "[Mulang] Updating version..."
 sed -i -r "s/version:             ${VERSION_REGEXP}/version:             ${NEW_VERSION}/" mulang.cabal
-sed -i -r "s/version = \"${VERSION_REGEXP}\"/version = \"${NEW_VERSION}\"/" app/Version.hs
-sed -i -r "s/VERSION = \"${VERSION_REGEXP}/VERSION = \"${NEW_VERSION}/" gem/lib/mulang/version.rb
-sed -i -r "s/MULANG_VERSION = \"${VERSION_REGEXP}/MULANG_VERSION = \"${NEW_VERSION}/" gem/lib/mulang/version.rb
-sed -i -r "s/\"version\": \"${VERSION_REGEXP}/\"version\": \"${NEW_VERSION}/" ghcjslib/package.json
-sed -i -r "s/MULANG_VERSION=${VERSION_REGEXP}/MULANG_VERSION=${NEW_VERSION}/" docs/devinit
+sed -i -r "s/version = \"${VERSION_REGEXP}\"/version = \"${NEW_VERSION}\"/"               app/Version.hs
+sed -i -r "s/VERSION = \"${VERSION_REGEXP}/VERSION = \"${NEW_VERSION}/"                   gem/lib/mulang/version.rb
+sed -i -r "s/MULANG_VERSION = \"${VERSION_REGEXP}/MULANG_VERSION = \"${NEW_VERSION}/"     gem/lib/mulang/version.rb
+sed -i -r "s/\"version\": \"${VERSION_REGEXP}/\"version\": \"${NEW_VERSION}/"             ghcjslib/package.json
+sed -i -r "s/VERSION = \"${VERSION_REGEXP}/VERSION = \"${NEW_VERSION}/"                   ghcjslib/gem/lib/mulangjs/version.rb
+sed -i -r "s/MULANG_VERSION = \"${VERSION_REGEXP}/MULANG_VERSION = \"${NEW_VERSION}/"     ghcjslib/gem/lib/mulangjs/version.rb
+sed -i -r "s/MULANG_VERSION=${VERSION_REGEXP}/MULANG_VERSION=${NEW_VERSION}/"             docs/devinit
 
 echo "[Mulang] Running tests..."
 stack test
@@ -31,7 +33,12 @@ echo "[Mulang] Deploying to NPM..."
 ./ghcjslib/swap.sh
 
 echo "[Mulang] Commiting files..."
-git commit mulang.cabal app/Version.hs gem/lib/mulang/version.rb gem/lib/mulang/version.rb ghcjslib/package.json docs/devinit -m "Welcome ${NEW_VERSION}!"
+git commit mulang.cabal \
+           app/Version.hs \
+           gem/lib/mulang/version.rb \
+           ghcjslib/package.json \
+           ghcjslib/gem/lib/mulangjs/version.rb \
+           docs/devinit -m "Welcome ${NEW_VERSION}!"
 
 echo "[Mulang] Tagging v$NEW_VERSION..."
 git tag "v${NEW_VERSION}"
