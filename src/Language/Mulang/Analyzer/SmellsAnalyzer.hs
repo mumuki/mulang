d423698af1dccecc59b96ecb441ca7b15f6159fb
Franco Bulgarelli <franco@mumuki.org>
2020-04-09 14:44:58 -0300

    Refactoring SmellsAnalyzer pipeline
    Now HasDeclarationTypos is properly supported

--- src/Language/Mulang/Analyzer/SmellsAnalyzer.hs
+++ src/Language/Mulang/Analyzer/SmellsAnalyzer.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE ViewPatterns, TupleSections #-}
 
 module Language.Mulang.Analyzer.SmellsAnalyzer (
   analyseSmells) where
@@ -9,24 +9,58 @@ import Language.Mulang.Inspector.Primitive
 import Language.Mulang.Inspector.Logic
 import Language.Mulang.Inspector.Generic.Smell
 import Language.Mulang.Inspector.Combiner (Location(..), locate)
-import Language.Mulang.Edl.Expectation (Query (..), CQuery (..), Matcher(..), Predicate(..))
+import Language.Mulang.Edl.Expectation (cQuery, CQuery (..), Matcher(..), Predicate(..))
 
 import Language.Mulang.Analyzer.Analysis hiding (DomainLanguage, Inspection, allSmells)
 
-import Data.List ((\\))
+import Data.List ((\\), intersect)
 import Data.Maybe (fromMaybe, mapMaybe)
 
+-- the runtime context of a smell analysis,
+-- which is composed of the previous expectations evaluation results and the domain language
+-- this context will be internally passed to core functions of this module
 type SmellsContext = ([QueryResult], DomainLanguage)
 
+-- An SmellInstance is an Smell type plus an optional target
+-- Most Smells have not target, but a few context-dependent smells
+-- may be instantiated multiple times with different targets
+-- before being evaluated
+type SmellInstance = (Smell, Maybe Identifier)
+
+-- A Detection is an executable representation of an SmellInstance
+type Detection = SmellsContext -> Expression -> [Identifier]
+
+-- Smell detection has three phases:
+--
+-- 1. Smells selection: dependening on the given SmellSet, differents lists of smells may be generated
+-- 2. Smells instantiation: for each selected smell, one ore more concrete smells will be instantiated
+-- 3. Smells evaluation: for each smell instance, it will be evaluated and zero or more Expectations will be synthesized
 analyseSmells :: Expression -> SmellsContext -> Maybe SmellsSet -> [Expectation]
-analyseSmells ast context = concatMap (\smell -> detectSmell smell context ast) . smellsFor . fromMaybe (NoSmells Nothing)
+analyseSmells ast context = concatMap (evalSmellInstance context ast) . concatMap (instantiateSmell context) . smellsFor
 
-detectSmell :: Smell -> SmellsContext -> Expression -> [Expectation]
-detectSmell smell context =  map (exectationFor smell) . detectionFor smell context
+instantiateSmell :: SmellsContext -> Smell -> [SmellInstance]
+instantiateSmell context smell = map (smell,) . targetsFor $ smell
+  where
+    targetsFor :: Smell -> [Maybe Identifier]
+    targetsFor "HasDeclarationTypos" = map Just . missingDeclarations . fst $ context
+    targetsFor _                     = [Nothing]
 
-smellsFor :: SmellsSet -> [Smell]
-smellsFor (NoSmells included)    = fromMaybe [] included
-smellsFor (AllSmells excluded)   = allSmells \\ (fromMaybe [] excluded)
+missingDeclarations :: [QueryResult] -> [Identifier]
+missingDeclarations = mapMaybe missingDeclaration
+  where
+    missingDeclaration (inspection -> Just ("Declares", name), (ExpectationResult _ False)) = Just name
+    missingDeclaration _                                                                    = Nothing
+
+    inspection (cQuery -> Just (Inspection name (Named arg) Unmatching)) = Just (name, arg)
+    inspection _                                                         = Nothing
+
+evalSmellInstance :: SmellsContext -> Expression -> SmellInstance -> [Expectation]
+evalSmellInstance context expression smellInstance =  map (exectationFor smellInstance) . detectionFor smellInstance context $ expression
+
+smellsFor :: Maybe SmellsSet -> [Smell]
+smellsFor (Just (NoSmells included))    = intersect allSmells (fromMaybe [] included)
+smellsFor (Just (AllSmells excluded))   = allSmells \\ (fromMaybe [] excluded)
+smellsFor _                             = []
 
 allSmells :: [Smell]
 allSmells = [
@@ -36,6 +70,7 @@ allSmells = [
   "DoesTypeTest",
   "HasAssignmentReturn",
   "HasCodeDuplication",
+  "HasDeclarationTypos",
   "HasEmptyIfBranches",
   "HasEmptyRepeat",
   "HasLongParameterList",
@@ -61,46 +96,44 @@ allSmells = [
   "UsesFail",
   "UsesUnificationOperator" ]
 
-detectionFor :: Smell -> Detection
-detectionFor "DiscardsExceptions"              = simple discardsExceptions
-detectionFor "DoesConsolePrint"                = simple doesConsolePrint
-detectionFor "DoesNilTest"                     = simple doesNilTest
-detectionFor "DoesNullTest"                    = simple doesNilTest
-detectionFor "DoesTypeTest"                    = simple doesTypeTest
-detectionFor "HasAssignmentReturn"             = simple hasAssignmentReturn
-detectionFor "HasCodeDuplication"              = unsupported
-detectionFor "HasEmptyIfBranches"              = simple hasEmptyIfBranches
-detectionFor "HasEmptyRepeat"                  = simple hasEmptyRepeat
-detectionFor "HasLongParameterList"            = simple hasLongParameterList
-detectionFor "HasMisspelledBindings"           = withLanguage hasMisspelledIdentifiers
-detectionFor "HasMisspelledIdentifiers"        = withLanguage hasMisspelledIdentifiers
-detectionFor "HasRedundantBooleanComparison"   = simple hasRedundantBooleanComparison
-detectionFor "HasRedundantGuards"              = simple hasRedundantGuards
-detectionFor "HasRedundantIf"                  = simple hasRedundantIf
-detectionFor "HasRedundantLambda"              = simple hasRedundantLambda
-detectionFor "HasRedundantLocalVariableReturn" = simple hasRedundantLocalVariableReturn
-detectionFor "HasRedundantParameter"           = simple hasRedundantParameter
-detectionFor "HasRedundantReduction"           = simple hasRedundantReduction
-detectionFor "HasRedundantRepeat"              = simple hasRedundantRepeat
-detectionFor "HasTooManyMethods"               = simple hasTooManyMethods
-detectionFor "HasTooShortBindings"             = withLanguage hasTooShortIdentifiers
-detectionFor "HasTooShortIdentifiers"          = withLanguage hasTooShortIdentifiers
-detectionFor "HasUnreachableCode"              = simple hasUnreachableCode
-detectionFor "HasWrongCaseBinding"             = withLanguage hasWrongCaseIdentifiers
-detectionFor "HasWrongCaseIdentifiers"         = withLanguage hasWrongCaseIdentifiers
-detectionFor "IsLongCode"                      = unsupported
-detectionFor "OverridesEqualOrHashButNotBoth"  = simple overridesEqualOrHashButNotBoth
-detectionFor "ShouldInvertIfCondition"         = simple shouldInvertIfCondition
-detectionFor "ReturnsNil"                      = simple returnsNil
-detectionFor "ReturnsNull"                     = simple returnsNil
-detectionFor "ShouldUseOtherwise"              = simple shouldUseOtherwise
-detectionFor "UsesCut"                         = simple usesCut
-detectionFor "UsesFail"                        = simple usesFail
-detectionFor "UsesUnificationOperator"         = simple usesUnificationOperator
-detectionFor "HasDeclarationTypos"             = failedDeclarations detectDeclarationTypos
-detectionFor _                                 = unsupported
-
-type Detection = SmellsContext -> Expression -> [Identifier]
+detectionFor :: SmellInstance -> Detection
+detectionFor ("DiscardsExceptions", Nothing)              = simple discardsExceptions
+detectionFor ("DoesConsolePrint", Nothing)                = simple doesConsolePrint
+detectionFor ("DoesNilTest", Nothing)                     = simple doesNilTest
+detectionFor ("DoesNullTest", Nothing)                    = simple doesNilTest
+detectionFor ("DoesTypeTest", Nothing)                    = simple doesTypeTest
+detectionFor ("HasAssignmentReturn", Nothing)             = simple hasAssignmentReturn
+detectionFor ("HasCodeDuplication", Nothing)              = unsupported
+detectionFor ("HasDeclarationTypos", Just target)         = raw (detectDeclarationTypos target)
+detectionFor ("HasEmptyIfBranches", Nothing)              = simple hasEmptyIfBranches
+detectionFor ("HasEmptyRepeat", Nothing)                  = simple hasEmptyRepeat
+detectionFor ("HasLongParameterList", Nothing)            = simple hasLongParameterList
+detectionFor ("HasMisspelledBindings", Nothing)           = withLanguage hasMisspelledIdentifiers
+detectionFor ("HasMisspelledIdentifiers", Nothing)        = withLanguage hasMisspelledIdentifiers
+detectionFor ("HasRedundantBooleanComparison", Nothing)   = simple hasRedundantBooleanComparison
+detectionFor ("HasRedundantGuards", Nothing)              = simple hasRedundantGuards
+detectionFor ("HasRedundantIf", Nothing)                  = simple hasRedundantIf
+detectionFor ("HasRedundantLambda", Nothing)              = simple hasRedundantLambda
+detectionFor ("HasRedundantLocalVariableReturn", Nothing) = simple hasRedundantLocalVariableReturn
+detectionFor ("HasRedundantParameter", Nothing)           = simple hasRedundantParameter
+detectionFor ("HasRedundantReduction", Nothing)           = simple hasRedundantReduction
+detectionFor ("HasRedundantRepeat", Nothing)              = simple hasRedundantRepeat
+detectionFor ("HasTooManyMethods", Nothing)               = simple hasTooManyMethods
+detectionFor ("HasTooShortBindings", Nothing)             = withLanguage hasTooShortIdentifiers
+detectionFor ("HasTooShortIdentifiers", Nothing)          = withLanguage hasTooShortIdentifiers
+detectionFor ("HasUnreachableCode", Nothing)              = simple hasUnreachableCode
+detectionFor ("HasWrongCaseBinding", Nothing)             = withLanguage hasWrongCaseIdentifiers
+detectionFor ("HasWrongCaseIdentifiers", Nothing)         = withLanguage hasWrongCaseIdentifiers
+detectionFor ("IsLongCode", Nothing)                      = unsupported
+detectionFor ("OverridesEqualOrHashButNotBoth", Nothing)  = simple overridesEqualOrHashButNotBoth
+detectionFor ("ShouldInvertIfCondition", Nothing)         = simple shouldInvertIfCondition
+detectionFor ("ReturnsNil", Nothing)                      = simple returnsNil
+detectionFor ("ReturnsNull", Nothing)                     = simple returnsNil
+detectionFor ("ShouldUseOtherwise", Nothing)              = simple shouldUseOtherwise
+detectionFor ("UsesCut", Nothing)                         = simple usesCut
+detectionFor ("UsesFail", Nothing)                        = simple usesFail
+detectionFor ("UsesUnificationOperator", Nothing)         = simple usesUnificationOperator
+detectionFor _                                      = unsupported
 
 unsupported :: Detection
 unsupported _ _ = []
@@ -111,26 +144,12 @@ simple inspection _ = locate' inspection
 withLanguage :: (DomainLanguage -> Inspection) -> Detection
 withLanguage inspection context = locate' (inspection (snd context))
 
-failedDeclarations :: (Identifier -> Expression -> [Identifier]) -> Detection
-failedDeclarations detection = undefined -- map (detection . missingDeclarations . fst
--- failedDeclarations inspection context = filter (\x -> y) . fst $ context
+raw :: (Expression -> [Identifier]) -> Detection
+raw detection _ = detection
 
-missingDeclarations :: [QueryResult] -> [Identifier]
-missingDeclarations = mapMaybe missingDeclaration
-  where
-    missingDeclaration (inspection -> Just ("Declares", name), (ExpectationResult _ False)) = Just name
-    missingDeclaration _                                                                    = Nothing
-
-    inspection (cQuery -> Just (Inspection name (Named arg) Unmatching)) = Just (name, arg)
-    inspection _                                                         = Nothing
-
-    cQuery (Decontextualize q) = Just q
-    cQuery (Within _ q)        = Just q
-    cQuery (Through _ q)       = Just q
-    cQuery _                   = Nothing
-
-exectationFor :: Smell -> Identifier -> Expectation
-exectationFor smell identifier = Expectation identifier smell
+exectationFor :: SmellInstance -> Identifier -> Expectation
+exectationFor (smell, Just target) identifier  = Expectation identifier (smell ++ ":" ++ target)
+exectationFor (smell, _          ) identifier  = Expectation identifier smell
 
 locate' inspection = identifiers . locate inspection
   where identifiers Nowhere      = []
