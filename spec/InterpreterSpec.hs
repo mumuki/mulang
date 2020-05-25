2dd3717b5c5830019b7e753d78589e6bd1c66f27
Franco Bulgarelli <franco@mumuki.org>
2020-05-25 12:04:17 -0300

    Simplifying test functions

--- spec/InterpreterSpec.hs
+++ spec/InterpreterSpec.hs
@@ -13,7 +13,7 @@ import           NeatInterpolation (text)
 
 lastRef result = (\(ref, ctx) -> (Map.! ref) . globalObjects $ ctx) <$> result
 
-run language code = eval defaultContext  . language . unpack $ code
+run language code = lastRef . eval defaultContext  . language . unpack $ code
 runjs = run js
 runpy = run py
 
@@ -22,48 +22,48 @@ spec = do
   describe "evalExpr" $ do
     context "javascript" $ do
       it "rejects logic on number" $ do
-        lastRef (runjs "1 || 2") `shouldThrow` (errorCall "Exception thrown outside try: Type error: expected two booleans but got (number) 1.0, (number) 2.0")
+        (runjs "1 || 2") `shouldThrow` (errorCall "Exception thrown outside try: Type error: expected two booleans but got (number) 1.0, (number) 2.0")
 
       it "rejects math on bools" $ do
-        lastRef (runjs "true + false") `shouldThrow` (errorCall "Exception thrown outside try: Type error: expected two numbers but got (boolean) true, (boolean) false")
+        (runjs "true + false") `shouldThrow` (errorCall "Exception thrown outside try: Type error: expected two numbers but got (boolean) true, (boolean) false")
 
       it "evals addition" $ do
-        lastRef (runjs "1 + 2") `shouldReturn` MuNumber 3
+        (runjs "1 + 2") `shouldReturn` MuNumber 3
 
       it "evals subtraction" $ do
-        lastRef (runjs "2 - 1") `shouldReturn` MuNumber 1
+        (runjs "2 - 1") `shouldReturn` MuNumber 1
 
       it "evals multiplication" $ do
-        lastRef (runjs "2 * 3") `shouldReturn` MuNumber 6
+        (runjs "2 * 3") `shouldReturn` MuNumber 6
 
       it "evals mod" $ do
-        lastRef (runjs "7 % 4") `shouldReturn` MuNumber 3
+        (runjs "7 % 4") `shouldReturn` MuNumber 3
 
       it "evals and" $ do
-        lastRef (runjs "true && true") `shouldReturn` MuBool True
+        (runjs "true && true") `shouldReturn` MuBool True
 
       it "evals or" $ do
-        lastRef (runjs "false || false") `shouldReturn` MuBool False
+        (runjs "false || false") `shouldReturn` MuBool False
 
       it "evals not" $ do
-        lastRef (runjs "!false") `shouldReturn` MuBool True
+        (runjs "!false") `shouldReturn` MuBool True
 
       it "evals comparison" $ do
-        lastRef (runjs "7 > 4") `shouldReturn` MuBool True
+        (runjs "7 > 4") `shouldReturn` MuBool True
 
       context "evals equal" $ do
         it "is false when values are different" $ do
-          lastRef (runjs "123 == 321") `shouldReturn` MuBool False
+          (runjs "123 == 321") `shouldReturn` MuBool False
 
         it "is true when values are the same" $ do
-          lastRef (runjs "'123' == '123'") `shouldReturn` MuBool True
+          (runjs "'123' == '123'") `shouldReturn` MuBool True
 
         it "is false when values are of different types" $ do
-          lastRef (runjs "'123' == 123") `shouldReturn` MuBool False
+          (runjs "'123' == 123") `shouldReturn` MuBool False
 
       context "evals if" $ do
         it "condition is true then evals first branch" $ do
-          lastRef (runjs [text|
+          (runjs [text|
             if(true){
               123
             } else {
@@ -71,7 +71,7 @@ spec = do
             }|]) `shouldReturn` MuNumber 123
 
         it "condition is false then evals second branch" $ do
-          lastRef (runjs [text|
+          (runjs [text|
             if(false){
               123
             } else {
@@ -79,31 +79,31 @@ spec = do
             }|]) `shouldReturn` MuNumber 456
 
         it "condition is non bool, fails" $ do
-          lastRef (runjs "if (6) { 123 } else { 456 }") `shouldThrow` (errorCall "Exception thrown outside try: Type error: expected boolean but got (number) 6.0")
+          (runjs "if (6) { 123 } else { 456 }") `shouldThrow` (errorCall "Exception thrown outside try: Type error: expected boolean but got (number) 6.0")
 
       it "evals functions" $ do
-        lastRef (runjs [text|
+        (runjs [text|
           function a() {
             return 123;
           }
           a()|]) `shouldReturn` MuNumber 123
 
       it "handles scopes" $ do
-        lastRef (runjs [text|
+        (runjs [text|
           function a() {
             function b(){}
           }
           b()|]) `shouldThrow` (errorCall "Exception thrown outside try: Reference not found for name 'b'")
 
       it "handles whiles" $ do
-        lastRef (runjs [text|
+        (runjs [text|
           var a = 0;
 
           while(a < 10) a = a + 1;
           a;|]) `shouldReturn` MuNumber 10
 
       it "handles fors" $ do
-        lastRef (runjs [text|
+        (runjs [text|
           var a = 0;
           var i = 0;
 
@@ -113,76 +113,76 @@ spec = do
 
     context "python" $ do
       it "evals addition" $ do
-        lastRef (runpy "1 + 2") `shouldReturn` MuNumber 3
+        (runpy "1 + 2") `shouldReturn` MuNumber 3
 
       it "evals subtraction" $ do
-        lastRef (runpy "2 - 1") `shouldReturn` MuNumber 1
+        (runpy "2 - 1") `shouldReturn` MuNumber 1
 
       it "evals multiplication" $ do
-        lastRef (runpy "2 * 3") `shouldReturn` MuNumber 6
-        lastRef (runpy "0 * 3") `shouldReturn` MuNumber 0
-        lastRef (runpy "0.5 * 6") `shouldReturn` MuNumber 3
+        (runpy "2 * 3") `shouldReturn` MuNumber 6
+        (runpy "0 * 3") `shouldReturn` MuNumber 0
+        (runpy "0.5 * 6") `shouldReturn` MuNumber 3
 
       it "evals multiplication with negative numbers" $ do
-        lastRef (runpy "-1 * 5") `shouldReturn` MuNumber (-5)
-        lastRef (runpy "(-1) * 5") `shouldReturn` MuNumber (-5)
-        lastRef (runpy "(-1) * (-8)") `shouldReturn` MuNumber 8
+        (runpy "-1 * 5") `shouldReturn` MuNumber (-5)
+        (runpy "(-1) * 5") `shouldReturn` MuNumber (-5)
+        (runpy "(-1) * (-8)") `shouldReturn` MuNumber 8
 
       it "evals division" $ do
-        lastRef (runpy "4 / 2") `shouldReturn` MuNumber 2
-        lastRef (runpy "4 / 0.5") `shouldReturn` MuNumber 8
+        (runpy "4 / 2") `shouldReturn` MuNumber 2
+        (runpy "4 / 0.5") `shouldReturn` MuNumber 8
 
       it "evals multiple multiplications" $ do
-        lastRef (runpy "2 * 3 * 10") `shouldReturn` MuNumber 60
-        lastRef (runpy "(2 * 3) * 10") `shouldReturn` MuNumber 60
-        lastRef (runpy "2 * (3 * 10)") `shouldReturn` MuNumber 60
+        (runpy "2 * 3 * 10") `shouldReturn` MuNumber 60
+        (runpy "(2 * 3) * 10") `shouldReturn` MuNumber 60
+        (runpy "2 * (3 * 10)") `shouldReturn` MuNumber 60
 
       it "evals mod" $ do
-        lastRef (runpy "7 % 4") `shouldReturn` MuNumber 3
+        (runpy "7 % 4") `shouldReturn` MuNumber 3
 
       it "evals and" $ do
-        lastRef (runpy "True and True") `shouldReturn` MuBool True
+        (runpy "True and True") `shouldReturn` MuBool True
 
       it "evals or" $ do
-        lastRef (runpy "False or False") `shouldReturn` MuBool False
+        (runpy "False or False") `shouldReturn` MuBool False
 
       it "evals comparison" $ do
-        lastRef (runpy "7 > 4") `shouldReturn` MuBool True
+        (runpy "7 > 4") `shouldReturn` MuBool True
 
       context "evals equal" $ do
         it "is false when values are different" $ do
-          lastRef (runpy "123 == 321") `shouldReturn` MuBool False
+          (runpy "123 == 321") `shouldReturn` MuBool False
 
         it "is true when values are the same" $ do
-          lastRef (runpy "'123' == '123'") `shouldReturn` MuBool True
+          (runpy "'123' == '123'") `shouldReturn` MuBool True
 
         it "is false when values are of different types" $ do
-          lastRef (runpy "'123' == 123") `shouldReturn` MuBool False
+          (runpy "'123' == 123") `shouldReturn` MuBool False
 
       context "evals if" $ do
         it "condition is true then evals first branch" $ do
-          lastRef (runpy [text|
+          (runpy [text|
             if True: 123
             else: 456|]) `shouldReturn` MuNumber 123
 
         it "condition is false then evals second branch" $ do
-          lastRef (runpy [text|
+          (runpy [text|
             if False: 123
             else: 456|]) `shouldReturn` MuNumber 456
 
         it "condition is non bool, fails" $ do
-          lastRef (runpy [text|
+          (runpy [text|
             if 6: 123
             else: 456|]) `shouldThrow` (errorCall "Exception thrown outside try: Type error: expected boolean but got (number) 6.0")
 
       it "evals functions" $ do
-        lastRef (runpy [text|
+        (runpy [text|
           def a():
             return 123
           a()|]) `shouldReturn` MuNumber 123
 
       it "evals functions with arguments" $ do
-        lastRef (runpy [text|
+        (runpy [text|
           def double(num):
             return 2 * num
 
@@ -191,7 +191,7 @@ spec = do
 
 
       it "evals functions that call other functions" $ do
-          lastRef (runpy [text|
+          (runpy [text|
             def double(num):
               return 2 * num
 
@@ -205,11 +205,11 @@ spec = do
           |]) `shouldReturn` MuNumber 12
 
       it "handles whiles" $ do
-        lastRef (runpy [text|
+        (runpy [text|
           a = 0
 
           while(a < 10): a = a + 1
           a|]) `shouldReturn` MuNumber 10
 
       it "evals not" $ do
-        lastRef (runpy "not False") `shouldReturn` MuBool True
+        (runpy "not False") `shouldReturn` MuBool True
