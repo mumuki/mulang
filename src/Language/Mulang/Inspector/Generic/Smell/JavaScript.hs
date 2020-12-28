49d82cde76fb2bc7a7e644679439329114d70b32
Franco Bulgarelli <franco@mumuki.org>
2020-12-27 21:50:21 -0300

    Introduce language-specific smells

---
+++ src/Language/Mulang/Inspector/Generic/Smell/JavaScript.hs
@@ -0,0 +1,12 @@
+module Language.Mulang.Inspector.Generic.Smell.JavaScript (
+  usesVarInsteadOfLet) where
+
+import           Language.Mulang.Ast
+import           Language.Mulang.Inspector.Primitive (Inspection, containsExpression)
+
+
+usesVarInsteadOfLet :: Inspection
+usesVarInsteadOfLet = containsExpression f
+  where f (Other (Just "var") _)                              = True
+        f (For [Generator (OtherPattern (Just "var") _) _] _) = True
+        f _                                                   = False
