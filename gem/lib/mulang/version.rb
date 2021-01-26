acc0229e07f93d750ed8ed1c0ae9528725bd87ba
Franco Bulgarelli <franco@mumuki.org>
2021-01-26 18:27:43 -0300

    Welcome 6.0.1!

--- gem/lib/mulang/version.rb
+++ gem/lib/mulang/version.rb
@@ -1,4 +1,4 @@
 module Mulang
-  VERSION = "6.0.0"
-  MULANG_VERSION = "6.0.0"
+  VERSION = "6.0.1"
+  MULANG_VERSION = "6.0.1"
 end
--- ghcjslib/gem/lib/mulangjs/version.rb
+++ ghcjslib/gem/lib/mulangjs/version.rb
@@ -1,4 +1,4 @@
 module MulangJs
-  VERSION = "6.0.0"
-  MULANG_VERSION = "6.0.0"
+  VERSION = "6.0.1"
+  MULANG_VERSION = "6.0.1"
 end
