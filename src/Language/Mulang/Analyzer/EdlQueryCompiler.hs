965ac6b1e2b6a847a80d365a11d27fd3112b1fee
Franco Leonardo Bulgarelli <franco@mumuki.org>
2021-01-28 14:41:48 -0300

    Allow compiler to take rules

--- src/Language/Mulang/Analyzer/EdlQueryCompiler.hs
+++ src/Language/Mulang/Analyzer/EdlQueryCompiler.hs
@@ -19,18 +19,25 @@ import qualified Language.Mulang.Edl.Expectation as E
 import Data.Maybe (fromMaybe)
 import Data.List.Split (splitOn)
 
+import           Data.Map (Map)
+import qualified Data.Map.Strict as Map
+
 type Scope = (ContextualizedInspection -> ContextualizedInspection, IdentifierPredicate -> IdentifierPredicate)
+type AutocorrectionRules = Map E.CQuery E.CQuery
 
 compileTopQuery :: E.Query -> Inspection
-compileTopQuery = fromMaybe (const True) . compileQuery
+compileTopQuery = compileTopQuery' Map.empty
 
-compileQuery :: E.Query -> Maybe Inspection
-compileQuery (E.Decontextualize query) = compileCQuery id query >>= (return . decontextualize)
-compileQuery (E.Within name query)     | (scope, p) <- compileWithin name  = fmap (decontextualize.scope) (compileCQuery p query)
-compileQuery (E.Through name query)    | (scope, p) <- compileThrough name = fmap (decontextualize.scope) (compileCQuery p query)
-compileQuery (E.Not query)             = fmap never (compileQuery query)
-compileQuery (E.And query1 query2)     = andAlso <$> (compileQuery query1) <*> (compileQuery query2)
-compileQuery (E.Or query1 query2)      = orElse <$> (compileQuery query1) <*> (compileQuery query2)
+compileTopQuery' :: AutocorrectionRules -> E.Query -> Inspection
+compileTopQuery' rules = fromMaybe (const True) . compileQuery rules
+
+compileQuery :: Map E.CQuery E.CQuery -> E.Query -> Maybe Inspection
+compileQuery rules (E.Decontextualize query) = compileCQuery rules id query >>= (return . decontextualize)
+compileQuery rules (E.Within name query)     | (scope, p) <- compileWithin name  = fmap (decontextualize.scope) (compileCQuery rules p query)
+compileQuery rules (E.Through name query)    | (scope, p) <- compileThrough name = fmap (decontextualize.scope) (compileCQuery rules p query)
+compileQuery rules (E.Not query)             = fmap never (compileQuery rules query)
+compileQuery rules (E.And query1 query2)     = andAlso <$> (compileQuery rules query1) <*> (compileQuery rules query2)
+compileQuery rules (E.Or query1 query2)      = orElse <$> (compileQuery rules query1) <*> (compileQuery rules query2)
 
 compileWithin, compileThrough :: String -> Scope
 compileWithin name  = scopeFor scopedList name
@@ -40,18 +47,18 @@ scopeFor :: ([Identifier] -> Inspection -> Inspection) -> Identifier -> Scope
 scopeFor f name = (contextualized (f names), andAlso (except (last names)))
   where names = splitOn "." name
 
-compileCQuery :: (IdentifierPredicate -> IdentifierPredicate) -> E.CQuery -> Maybe ContextualizedInspection
-compileCQuery pm (E.Inspection i p m) = ($ (compilePredicate pm p))         <$> compileInspection (compileVerb i) m
-compileCQuery pm (E.AtLeast n q)      = contextualized (atLeast (encode n)) <$> compileTQuery pm q
-compileCQuery pm (E.AtMost n q)       = contextualized (atMost (encode n))  <$> compileTQuery pm q
-compileCQuery pm (E.Exactly n q)      = contextualized (exactly (encode n)) <$> compileTQuery pm q
-compileCQuery pm (E.CNot q)           = contextualized never                <$> compileCQuery pm q
-compileCQuery pm (E.CAnd q1 q2)       = contextualized2 andAlso             <$> compileCQuery pm q1 <*> compileCQuery pm q2
-compileCQuery pm (E.COr q1 q2)        = contextualized2 orElse              <$> compileCQuery pm q1 <*> compileCQuery pm q2
+compileCQuery :: AutocorrectionRules -> (IdentifierPredicate -> IdentifierPredicate) -> E.CQuery -> Maybe ContextualizedInspection
+compileCQuery rules pm (E.Inspection i p m) = ($ (compilePredicate pm p))         <$> compileInspection rules (compileVerb i) m
+compileCQuery rules pm (E.AtLeast n q)      = contextualized (atLeast (encode n)) <$> compileTQuery rules pm q
+compileCQuery rules pm (E.AtMost n q)       = contextualized (atMost (encode n))  <$> compileTQuery rules pm q
+compileCQuery rules pm (E.Exactly n q)      = contextualized (exactly (encode n)) <$> compileTQuery rules pm q
+compileCQuery rules pm (E.CNot q)           = contextualized never                <$> compileCQuery rules pm q
+compileCQuery rules pm (E.CAnd q1 q2)       = contextualized2 andAlso             <$> compileCQuery rules pm q1 <*> compileCQuery rules pm q2
+compileCQuery rules pm (E.COr q1 q2)        = contextualized2 orElse              <$> compileCQuery rules pm q1 <*> compileCQuery rules pm q2
 
-compileTQuery :: (IdentifierPredicate -> IdentifierPredicate) -> E.TQuery -> Maybe ContextualizedCounter
-compileTQuery pm (E.Counter i p m) = ($ (compilePredicate pm p)) <$> compileCounter (compileVerb i) m
-compileTQuery pm (E.Plus q1 q2)    = contextualized2 plus        <$> (compileTQuery pm q1) <*> (compileTQuery pm q2)
+compileTQuery :: AutocorrectionRules -> (IdentifierPredicate -> IdentifierPredicate) -> E.TQuery -> Maybe ContextualizedCounter
+compileTQuery rules pm (E.Counter i p m) = ($ (compilePredicate pm p)) <$> compileCounter rules (compileVerb i) m
+compileTQuery rules pm (E.Plus q1 q2)    = contextualized2 plus        <$> (compileTQuery rules pm q1) <*> (compileTQuery rules pm q2)
 
 compilePredicate :: (IdentifierPredicate -> IdentifierPredicate) -> E.Predicate -> IdentifierPredicate
 compilePredicate p E.Any             = p $ anyone
@@ -69,42 +76,42 @@ compileVerb :: String -> String
 compileVerb = concat . map headToUpper . words
   where headToUpper (x:xs) = toUpper x : xs
 
-compileCounter :: String -> E.Matcher -> Maybe (ContextualizedBoundCounter)
-compileCounter = f
+compileCounter :: AutocorrectionRules -> String -> E.Matcher -> Maybe (ContextualizedBoundCounter)
+compileCounter rules = f
   where
-  f "Calls"                      m            = boundMatching countCalls m
-  f "DeclaresAttribute"          m            = boundMatching countAttributes m
-  f "DeclaresClass"              m            = boundMatching countClasses m
-  f "DeclaresFunction"           m            = boundMatching countFunctions m
-  f "DeclaresInterface"          m            = boundMatching countInterfaces m
-  f "DeclaresMethod"             m            = boundMatching countMethods m
-  f "DeclaresObject"             m            = boundMatching countObjects m
-  f "DeclaresProcedure"          m            = boundMatching countProcedures m
-  f "DeclaresVariable"           m            = boundMatching countVariables m
-  f "Returns"                    m            = plainMatching countReturns m
+  f "Calls"                      m            = boundMatching rules countCalls m
+  f "DeclaresAttribute"          m            = boundMatching rules countAttributes m
+  f "DeclaresClass"              m            = boundMatching rules countClasses m
+  f "DeclaresFunction"           m            = boundMatching rules countFunctions m
+  f "DeclaresInterface"          m            = boundMatching rules countInterfaces m
+  f "DeclaresMethod"             m            = boundMatching rules countMethods m
+  f "DeclaresObject"             m            = boundMatching rules countObjects m
+  f "DeclaresProcedure"          m            = boundMatching rules countProcedures m
+  f "DeclaresVariable"           m            = boundMatching rules countVariables m
+  f "Returns"                    m            = plainMatching rules countReturns m
   f "Uses"                       E.Unmatching = bound countUses
-  f "UsesFor"                    m            = plainMatching countFors m
-  f "UsesForEach"                m            = plainMatching countForEaches m
-  f "UsesForLoop"                m            = plainMatching countForLoops m
-  f "UsesIf"                     m            = plainMatching countIfs m
-  f "UsesLambda"                 m            = plainMatching countLambdas m
-  f "UsesPrint"                  m            = plainMatching countPrints m
-  f "UsesRepeat"                 m            = plainMatching countRepeats m
-  f "UsesSwitch"                 m            = plainMatching countSwitches m
-  f "UsesWhile"                  m            = plainMatching countWhiles m
-  f "UsesYield"                  m            = plainMatching countYiels m
+  f "UsesFor"                    m            = plainMatching rules countFors m
+  f "UsesForEach"                m            = plainMatching rules countForEaches m
+  f "UsesForLoop"                m            = plainMatching rules countForLoops m
+  f "UsesIf"                     m            = plainMatching rules countIfs m
+  f "UsesLambda"                 m            = plainMatching rules countLambdas m
+  f "UsesPrint"                  m            = plainMatching rules countPrints m
+  f "UsesRepeat"                 m            = plainMatching rules countRepeats m
+  f "UsesSwitch"                 m            = plainMatching rules countSwitches m
+  f "UsesWhile"                  m            = plainMatching rules countWhiles m
+  f "UsesYield"                  m            = plainMatching rules countYiels m
   f (primitiveUsage -> Just p)   E.Unmatching = plain (countUsesPrimitive p)
   f _                            _            = Nothing
 
 
-compileInspection :: String -> E.Matcher -> Maybe ContextualizedBoundInspection
-compileInspection = f
+compileInspection :: AutocorrectionRules -> String -> E.Matcher -> Maybe ContextualizedBoundInspection
+compileInspection rules = f
   where
-  f "Assigns"                          m              = boundMatching assignsMatching m
-  f "Calls"                            m              = boundMatching callsMatching m
+  f "Assigns"                          m              = boundMatching rules assignsMatching m
+  f "Calls"                            m              = boundMatching rules callsMatching m
   f "Declares"                         E.Unmatching   = bound declares
-  f "DeclaresAttribute"                m              = boundMatching declaresAttributeMatching m
-  f "DeclaresClass"                    m              = boundMatching declaresClassMatching m
+  f "DeclaresAttribute"                m              = boundMatching rules declaresAttributeMatching m
+  f "DeclaresClass"                    m              = boundMatching rules declaresClassMatching m
   f "DeclaresComputation"              E.Unmatching   = bound declaresComputation
   f "DeclaresComputationWithArity0"    E.Unmatching   = bound (declaresComputationWithArity 0)
   f "DeclaresComputationWithArity1"    E.Unmatching   = bound (declaresComputationWithArity 1)
@@ -112,21 +119,21 @@ compileInspection = f
   f "DeclaresComputationWithArity3"    E.Unmatching   = bound (declaresComputationWithArity 3)
   f "DeclaresComputationWithArity4"    E.Unmatching   = bound (declaresComputationWithArity 4)
   f "DeclaresComputationWithArity5"    E.Unmatching   = bound (declaresComputationWithArity 5)
-  f "DeclaresEntryPoint"               m              = boundMatching declaresEntryPointMatching m
+  f "DeclaresEntryPoint"               m              = boundMatching rules declaresEntryPointMatching m
   f "DeclaresEnumeration"              E.Unmatching   = bound declaresEnumeration
   f "DeclaresFact"                     E.Unmatching   = bound declaresFact
-  f "DeclaresFunction"                 m              = boundMatching declaresFunctionMatching m
-  f "DeclaresInterface"                m              = boundMatching declaresInterfaceMatching m
-  f "DeclaresMethod"                   m              = boundMatching declaresMethodMatching m
-  f "DeclaresObject"                   m              = boundMatching declaresObjectMatching m
+  f "DeclaresFunction"                 m              = boundMatching rules declaresFunctionMatching m
+  f "DeclaresInterface"                m              = boundMatching rules declaresInterfaceMatching m
+  f "DeclaresMethod"                   m              = boundMatching rules declaresMethodMatching m
+  f "DeclaresObject"                   m              = boundMatching rules declaresObjectMatching m
   f "DeclaresPredicate"                E.Unmatching   = bound declaresPredicate
-  f "DeclaresProcedure"                m              = boundMatching declaresProcedureMatching m
+  f "DeclaresProcedure"                m              = boundMatching rules declaresProcedureMatching m
   f "DeclaresRecursively"              E.Unmatching   = bound declaresRecursively
   f "DeclaresRule"                     E.Unmatching   = bound declaresRule
   f "DeclaresSuperclass"               E.Unmatching   = bound declaresSuperclass
   f "DeclaresTypeAlias"                E.Unmatching   = bound declaresTypeAlias
   f "DeclaresTypeSignature"            E.Unmatching   = bound declaresTypeSignature
-  f "DeclaresVariable"                 m              = boundMatching declaresVariableMatching m
+  f "DeclaresVariable"                 m              = boundMatching rules declaresVariableMatching m
   f "Delegates"                        E.Unmatching   = contextualizedBound delegates'
   f "Implements"                       E.Unmatching   = bound implements
   f "Includes"                         E.Unmatching   = bound includes
@@ -142,7 +149,7 @@ compileInspection = f
   f "IsLiteral"                        E.Unmatching   = plain isLiteral
   f "Raises"                           E.Unmatching   = bound raises
   f "Rescues"                          E.Unmatching   = bound rescues
-  f "Returns"                          m              = plainMatching returnsMatching m
+  f "Returns"                          m              = plainMatching rules returnsMatching m
   f "SubordinatesDeclarationsTo"       E.Unmatching   = bound subordinatesDeclarationsTo
   f "SubordinatesDeclarationsToEntryPoint" E.Unmatching = plain subordinatesDeclarationsToEntryPoint
   f "TypesAs"                          E.Unmatching   = bound typesAs
@@ -158,15 +165,15 @@ compileInspection = f
   f "UsesExceptionHandling"            E.Unmatching   = plain usesExceptionHandling
   f "UsesExceptions"                   E.Unmatching   = plain usesExceptions
   f "UsesFindall"                      E.Unmatching   = plain usesFindall
-  f "UsesFor"                          m              = plainMatching usesForMatching m
+  f "UsesFor"                          m              = plainMatching rules usesForMatching m
   f "UsesForall"                       E.Unmatching   = plain usesForall
   f "UsesForComprehension"             E.Unmatching   = plain usesForComprehension
-  f "UsesForeach"                      m              = plainMatching usesForEachMatching m
-  f "UsesForLoop"                      m              = plainMatching usesForLoopMatching m
+  f "UsesForeach"                      m              = plainMatching rules usesForEachMatching m
+  f "UsesForLoop"                      m              = plainMatching rules usesForLoopMatching m
   f "UsesGuards"                       E.Unmatching   = plain usesGuards
-  f "UsesIf"                           m              = plainMatching usesIfMatching m
+  f "UsesIf"                           m              = plainMatching rules usesIfMatching m
   f "UsesInheritance"                  E.Unmatching   = plain usesInheritance
-  f "UsesLambda"                       m              = plainMatching usesLambdaMatching m
+  f "UsesLambda"                       m              = plainMatching rules usesLambdaMatching m
   f "UsesLogic"                        E.Unmatching   = plain usesLogic
   f "UsesLoop"                         E.Unmatching   = plain usesLoop
   f "UsesMath"                         E.Unmatching   = plain usesMath
@@ -174,15 +181,15 @@ compileInspection = f
   f "UsesNot"                          E.Unmatching   = plain usesNot
   f "UsesObjectComposition"            E.Unmatching   = plain usesObjectComposition
   f "UsesPatternMatching"              E.Unmatching   = plain usesPatternMatching
-  f "UsesPrint"                        m              = plainMatching usesPrintMatching m
-  f "UsesRepeat"                       m              = plainMatching usesRepeatMatching m
+  f "UsesPrint"                        m              = plainMatching rules usesPrintMatching m
+  f "UsesRepeat"                       m              = plainMatching rules usesRepeatMatching m
   f "UsesStaticMethodOverload"         E.Unmatching   = plain usesStaticMethodOverload
   f "UsesStaticPolymorphism"           E.Unmatching   = contextual usesStaticPolymorphism'
-  f "UsesSwitch"                       m              = plainMatching usesSwitchMatching m
+  f "UsesSwitch"                       m              = plainMatching rules usesSwitchMatching m
   f "UsesTemplateMethod"               E.Unmatching   = plain usesTemplateMethod
   f "UsesType"                         E.Unmatching   = bound usesType
-  f "UsesWhile"                        m              = plainMatching usesWhileMatching m
-  f "UsesYield"                        m              = plainMatching usesYieldMatching m
+  f "UsesWhile"                        m              = plainMatching rules usesWhileMatching m
+  f "UsesYield"                        m              = plainMatching rules usesYieldMatching m
   f (primitiveDeclaration -> Just p)   E.Unmatching   = plain (declaresPrimitive p)
   f (primitiveUsage -> Just p)         E.Unmatching   = plain (usesPrimitive p)
   f (primitiveEssence -> Just p)       E.Unmatching   = plain (isPrimitive p)
@@ -204,18 +211,18 @@ plain = Just . contextualizedBind . contextualize
 bound :: BoundConsult a -> Maybe (ContextualizedBoundConsult a)
 bound = Just . boundContextualize
 
-boundMatching :: (Matcher -> BoundConsult a) -> E.Matcher -> Maybe (ContextualizedBoundConsult a)
-boundMatching f m = bound (f (compileMatcher m))
+boundMatching :: AutocorrectionRules -> (Matcher -> BoundConsult a) -> E.Matcher -> Maybe (ContextualizedBoundConsult a)
+boundMatching rules f m = bound (f (compileMatcher rules m))
 
-plainMatching :: (Matcher -> Consult a) -> E.Matcher -> Maybe (ContextualizedBoundConsult a)
-plainMatching f m = plain (f (compileMatcher m))
+plainMatching :: AutocorrectionRules -> (Matcher -> Consult a) -> E.Matcher -> Maybe (ContextualizedBoundConsult a)
+plainMatching rules f m = plain (f (compileMatcher rules m))
 
-compileMatcher :: E.Matcher -> Matcher
-compileMatcher (E.Matching clauses) = compileClauses clauses
-compileMatcher _                    = const True
+compileMatcher :: AutocorrectionRules -> E.Matcher -> Matcher
+compileMatcher rules (E.Matching clauses) = compileClauses rules clauses
+compileMatcher _     _                    = const True
 
-compileClauses :: [E.Clause] -> Matcher
-compileClauses = withEvery . f
+compileClauses :: AutocorrectionRules -> [E.Clause] -> Matcher
+compileClauses rules = withEvery . f
   where
     f :: [E.Clause] -> [Inspection]
     f (E.IsAnything:args)       = isAnything : (f args)
@@ -231,5 +238,5 @@ compileClauses = withEvery . f
     f (E.IsNumber value:args)   = isNumber value : (f args)
     f (E.IsString value:args)   = isString value : (f args)
     f (E.IsSymbol value:args)   = isSymbol value : (f args)
-    f (E.That expectation:args) = compileTopQuery expectation : (f args)
+    f (E.That expectation:args) = compileTopQuery' rules expectation : (f args)
     f []                        = []
